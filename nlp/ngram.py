1461e563e4d6e963ae3ebdc3f52ac9978a9ac224
Chirag S. Jamadagni <chirags.jamadagni@lawn-128-61-78-100.lawn.gatech.edu>
2018-01-18 16:02:52 -0500

    cleaning up ngram code

--- nlp/ngram.py
+++ nlp/ngram.py
@@ -3,11 +3,13 @@ n-gram generator for Chart review
 
 Usage python ngram.py <n_value> <lookup_term>
 
+Google: nltk collocations for more filtering and approaches
+
 """
 
 
-import sys
-import re
+#import sys
+#import re
 import nltk
 from nltk import word_tokenize
 from nltk import sent_tokenize
@@ -17,8 +19,8 @@ from collections import Counter
 
 
 # Inputs for testing
-n = 2
-word = None
+n = 4
+word = 'through'
 
 # For texting using text file.
 # TODO: Extract information from Solr
@@ -27,7 +29,8 @@ with open('input.txt') as f:
 f.close()
 
 if word:
-    sentences = text.split('.')
+    #sentences = text.split('.')
+    sentences = nltk.sent_tokenize(text)
     sentence_list = [x for x in sentences if word in x]
     text = ''.join(sentence_list)
 
@@ -44,5 +47,27 @@ result = [x for x in all_ngrams if word in x]
 # Printing ngrams and their frequences
 fdist = nltk.FreqDist(all_ngrams)
 for i,j in fdist.items():
-    if j>1:
+    if j>2:
         print (i,j)
+
+
+
+def extract_ngrams(cohort_id, keyword='NIL', n=3, frequency=1):
+    # GET DATA
+    # text = ?
+    if keyword != 'NIL':
+        sentences = nltk.sent_tokenize(text)
+        sentence_list = [x for x in sentences if word in x]
+        text = ''.join(sentence_list)
+
+    token = RegexpTokenizer(r'\w+') #without Punctuations
+    token = token.tokenize(text)
+    all_ngrams = ngrams(token, n)
+
+    result = []
+    fdist = nltk.FreqDist(all_ngrams)
+    for i,j in fdist.items():
+        if j>frequency:
+            result.append((i,j))
+
+    return result
