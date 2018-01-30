2555e7afebbd34754722dd98017fc2089007f8dc
Charity Hilton <Charity.Hilton@gtri.gatech.edu>
2018-01-30 14:51:36 -0500

    add logging for func calls

---
+++ apache.md
@@ -0,0 +1,3 @@
+TODO wsgi
+
+setsebool -P httpd_can_network_connect 1
--- luigi_pipeline_runner.py
+++ luigi_pipeline_runner.py
@@ -10,5 +10,9 @@ luigi_log = util.log_dir + '/luigi.log'
 def run_pipeline(pipeline_type: str, pipeline_id: str, job_id: int, owner: str):
     # after many efforts to use threading + the luigi API, can't seem to get it to work
     # this solution seems to work ok for now, may want to revisit later
-    func = "PYTHONPATH='.' %s --module luigi_pipeline %s --pipeline %s --job %s --owner %s > %s 2>&1 &" % (util.luigi_home, pipeline_types[pipeline_type],pipeline_id, str(job_id), owner, luigi_log)
-    call(func, shell=True)
+    func = "PYTHONPATH='.' %s --module luigi_pipeline %s --pipeline %s --job %s --owner %s > %s 2>&1 &" % (util.luigi_home, pipeline_types[pipeline_type], pipeline_id, str(job_id), owner, luigi_log)
+    try:
+        call(func, shell=True)
+    except Exception as ex:
+        print(ex)
+        print("unable to execute %s" % func)
