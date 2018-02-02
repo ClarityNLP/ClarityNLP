f9476fb0132821714e163eecc67fe62b421dd37a
cjamadagni3 <cjamadagni@gatech.edu>
2018-02-02 15:44:40 -0500

    Code template for vocabulary.py

--- nlp/vocabulary.py
+++ nlp/vocabulary.py
@@ -5,3 +5,25 @@ Vocabulary Expansion
 - ancestors
 - descendants
 """
+
+# Function to get synonyms for given concept
+def get_synonyms(conn_string, concept):
+    conn = psycopg2.connect(conn_string)
+    cursor = conn.cursor()
+
+    #TODO: Queries
+# Port queries present at https://github.gatech.edu/HDAP/omop-query/blob/master/src/main/scala/gtri/nlp/omop_query/Queries.scala
+
+# Function to get ancestors for given concept
+def get_ancestors(conn_string, concept):
+    conn = psycopg2.connect(conn_string)
+    cursor = conn.cursor()
+
+    #TODO: Queries
+
+# Function to get descendants for given concept
+def get_descendants(conn_string, concept):
+    conn = psycopg2.connect(conn_string)
+    cursor = conn.cursor()
+
+    #TODO: Queries
