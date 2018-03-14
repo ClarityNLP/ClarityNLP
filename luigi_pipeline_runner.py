661f2027731aa50179a1f164f2217634f6bf5bd1
Charity Hilton <Charity.Hilton@gtri.gatech.edu>
2018-03-14 14:38:22 -0400

    modify luigi runner to run in background

--- luigi_pipeline_runner.py
+++ luigi_pipeline_runner.py
@@ -1,6 +1,7 @@
 from subprocess import call
 import util
 import sys
+
 # this maps the user friendly name to the pipeline task in luigi_pipeline.py
 pipeline_types = {
     "TermFinder": "TermFinderPipeline",
@@ -11,8 +12,9 @@ luigi_log = util.log_dir + '/luigi.log'
 
 def run_pipeline(pipeline_type: str, pipeline_id: str, job_id: int, owner: str):
     scheduler = util.luigi_scheduler
-    func = "PYTHONPATH='.' luigi --workers %s --module luigi_pipeline %s --pipeline %s --job %s --owner %s --scheduler-url %s" % (
-        str(util.luigi_workers), pipeline_types[pipeline_type], pipeline_id, str(job_id), owner, scheduler)
+    print("running job %s on pipeline %s; logging here %s" % (str(job_id), str(pipeline_id), luigi_log))
+    func = "PYTHONPATH='.' luigi --workers %s --module luigi_pipeline %s --pipeline %s --job %s --owner %s --scheduler-url %s > %s 2>&1 &" % (
+        str(util.luigi_workers), pipeline_types[pipeline_type], pipeline_id, str(job_id), owner, scheduler, luigi_log)
     try:
         call(func, shell=True)
     except Exception as ex:
