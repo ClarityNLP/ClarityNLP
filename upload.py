4d2fdac438223a3daef411a18cbdd24fd4ebf6b3
cjamadagni3 <cjamadagni@gatech.edu>
2018-03-28 14:27:29 -0400

    Adding support for interventions table

--- upload.py
+++ upload.py
@@ -74,6 +74,8 @@ def upload_from_db(conn_string, solr_url):
     else:
         responseMsg = "Could not upload. Contact Admin."
 
+    conn.close()
+
     return responseMsg
 
 def aact_db_upload(solr_url):
@@ -88,13 +90,14 @@ def aact_db_upload(solr_url):
     cursor = conn.cursor()
 
     # SOLR upload headers
-    #url = solr_url_2 + '/update/json'
+    #url = solr_url + '/update/json'
     url = solr_url + '/update?commit=true'
     headers = {
     'Content-type': 'application/json',
     }
 
-    # Extracting information
+
+    # Extracting information - detailed_descriptions
     cursor.execute("""SELECT * FROM detailed_descriptions INNER JOIN studies ON studies.nct_id = detailed_descriptions.nct_id WHERE studies.first_received_date > '2018-01-01' LIMIT 1000""")
     result = cursor.fetchall()
 
@@ -102,7 +105,7 @@ def aact_db_upload(solr_url):
     for i in result:
         d = {"subject":i[1],
             "description_attr":"AACT Clinical Trials",
-            "source":"AACT Clinical Trials",
+            "source":"AACT",
             "report_type":"Clinical Trial Description",
             "report_text":i[2],
             "cg_id": "",
@@ -121,10 +124,40 @@ def aact_db_upload(solr_url):
     data = json.dumps(result_list)
     response = requests.post(url, headers=headers, data=data)
 
+
+    # Extracting information - Interventions
+    cursor.execute("""SELECT * FROM interventions INNER JOIN studies ON studies.nct_id = interventions.nct_id WHERE studies.first_received_date > '2018-01-01' AND interventions.description IS NOT NULL LIMIT 1000 """)
+    result = cursor.fetchall()
+
+    result_list = []
+    for i in result:
+        d = {"subject":i[1],
+            "description_attr":"AACT Clinical Trials",
+            "source":"AACT",
+            "report_type":"Clinical Trial Interventions",
+            "report_text": str(i[4]),
+            "cg_id": "",
+            "report_id": i[0],
+            "is_error_attr": "",
+            "id": i[0],
+            "store_time_attr": "",
+            "chart_time_attr": "",
+            "admission_id": "",
+            "report_date": str(i[7])
+            }
+
+        result_list.append(d)
+
+    # Pushing data to Solr
+    data = json.dumps(result_list)
+    response2 = requests.post(url, headers=headers, data=data)
+
     # Result verification
-    if response.status_code == 200:
+    if response.status_code == 200 and response2.status_code == 200:
         response_msg = "Successfully migrated data to Solr."
     else:
         response_msg = "Could not upload. Contact Admin."
+
+    conn.close()
 
     return response_msg
