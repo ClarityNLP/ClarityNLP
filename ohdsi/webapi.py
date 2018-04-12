8007c67ae0c5a11b2773353f2c06fa4758e9833c
cjamadagni3 <cjamadagni@gatech.edu>
2018-04-12 14:02:04 -0400

    getCohort() API

--- ohdsi/webapi.py
+++ ohdsi/webapi.py
@@ -3,3 +3,32 @@ OHDSI Helpers
 """
 
 import requests
+import json
+
+
+ENDPOINT = 'https://apps.hdap.gatech.edu/ohdsi/WebAPI'
+
+def getCohort(cohort_id):
+
+    # Getting the cohort summary
+    url = ENDPOINT + '/cohortanalysis/%s/summary' %(cohort_id)
+    cohort_details = requests.get(url).json()
+
+    # Getting list of patients in the cohort
+    url = ENDPOINT + '/cohort/%s' %(cohort_id)
+    cohort_patients = requests.get(url).json()
+
+    cohort = {'Details':cohort_details,'Patients':cohort_patients}
+
+    return json.dumps(cohort)
+
+
+    # print (data['cohortDefinition'])
+    #
+    # for key, val in cohort.items():
+    #     print (key)
+
+
+
+if __name__ == '__main__':
+    getCohort(6)
