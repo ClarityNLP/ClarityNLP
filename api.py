178114d31b8d149f255be011c9020c4206342880
cjamadagni3 <cjamadagni@gatech.edu>
2018-04-17 14:49:01 -0400

    Removing stray print statement

--- api.py
+++ api.py
@@ -45,7 +45,6 @@ def ohdsi_patient_event():
     """Get patient event details."""
     if request.method == 'POST':
         r = request.get_json()
-        print (tuple(r['concepts']))
         patient_event_details = getPatientEvent(r['cohort_id'], r['domain'], tuple(r['concepts']), util.conn_string_2)
 
         return patient_event_details
