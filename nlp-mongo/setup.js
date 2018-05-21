7c98a82e239cccd9c667d792fd93334e5ea3ed54
Trey Schneider <trey@codeschneider.com>
2018-05-21 13:05:39 -0400

    remove mapper-api, mapper-client, nlp-mongo, nlp-postgres, and nlp-solr from top level

--- nlp-mongo/setup.js
+++
@@ -1,4 +0,0 @@
-db.pipeline_results.createIndex( {  "subject":1 })
-db.pipeline_results.createIndex( {  "job_id":1 })
-db.pipeline_results.createIndex( {  "nlpql_name":1 })
-db.pipeline_results.createIndex( {  "pipeline_id":1  })
