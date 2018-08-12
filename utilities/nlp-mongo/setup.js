575e5786d5df56145293186f6f0e023721dadbb8
Charity Hilton <Charity.Hilton@gtri.gatech.edu>
2018-08-12 15:47:21 -0400

    add indexes to mongo phenotype_results

--- utilities/nlp-mongo/setup.js
+++ utilities/nlp-mongo/setup.js
@@ -2,3 +2,9 @@ db.pipeline_results.createIndex( {  "subject":1 })
 db.pipeline_results.createIndex( {  "job_id":1 })
 db.pipeline_results.createIndex( {  "nlpql_name":1 })
 db.pipeline_results.createIndex( {  "pipeline_id":1  })
+
+
+db.phenotype_results.createIndex( {  "subject":1 })
+db.phenotype_results.createIndex( {  "job_id":1 })
+db.phenotype_results.createIndex( {  "nlpql_feature":1 })
+db.phenotype_results.createIndex( {  "pipeline_id":1  })
