c19374fda73fa7ec8ed44f35dfa2616fa45c901d
Jon Duke <jduke99@gmail.com>
2018-10-23 23:25:19 -0400

    New Custom Tasks

--- nlp/custom_tasks/AzureSentimentTask.py
+++ nlp/custom_tasks/AzureSentimentTask.py
@@ -1,10 +1,8 @@
 from tasks.task_utilities import BaseTask
 from pymongo import MongoClient
 import requests
-import util
 
-
-class SentimentAPITask(BaseTask):
+class AzureSentimentTask(BaseTask):
     task_name = "AzureSentiment"
 
     # NLPQL
@@ -21,22 +19,23 @@ class SentimentAPITask(BaseTask):
             sentence_list = self.get_document_sentences(doc)
 
             for sentence in sentence_list:
-                headers = {'Content-Type': 'application/json', 'Ocp-Apim-Subscription-Key': self.pipeline_config.custom_arguments['api_key']}
-                self.write_log_data("INFO", self.pipeline_config.custom_arguments['api_key'])
+                if any(word.lower() in sentence.lower() for word in self.pipeline_config.terms):
+                    headers = {'Content-Type': 'application/json', 'Ocp-Apim-Subscription-Key': self.pipeline_config.custom_arguments['api_key']}
+                    self.write_log_data("INFO", self.pipeline_config.custom_arguments['api_key'])
 
-                payload = {"documents": [{"language": "en", "id": "1", "text": sentence}]}
-                response = requests.post('https://eastus.api.cognitive.microsoft.com/text/analytics/v2.0/sentiment', headers=headers, json=payload)
-                if response.status_code == 200:
-                    json_response = response.json()
-                    val = json_response['documents'][0]
-                    obj = {
-                        'sentiment_score': val['score'],
-                        'sentence': sentence
-                    }
+                    payload = {"documents": [{"language": "en", "id": "1", "text": sentence}]}
+                    response = requests.post('https://eastus.api.cognitive.microsoft.com/text/analytics/v2.0/sentiment', headers=headers, json=payload)
+                    if response.status_code == 200:
+                        json_response = response.json()
+                        val = json_response['documents'][0]
+                        obj = {
+                            'sentiment_score': val['score'],
+                            'sentence': sentence
+                        }
 
-                    # writing results
-                    self.write_result_data(temp_file, mongo_client, doc, obj)
+                        # writing results
+                        self.write_result_data(temp_file, mongo_client, doc, obj)
 
-                else:
-                    # writing to log (optional)
-                    self.write_log_data("OOPS", "No sentiment this time!")
+                    else:
+                        # writing to log (optional)
+                        self.write_log_data("OOPS", "No sentiment this time!")
