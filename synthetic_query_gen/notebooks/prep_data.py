263caffefaa569f0243206c9d35b7dea0b207a68
Charity Hilton <Charity.Hilton@gtri.gatech.edu>
2018-10-24 18:39:40 -0400

    add loading from piped files into solr

--- synthetic_query_gen/notebooks/prep_data.py
+++ synthetic_query_gen/notebooks/prep_data.py
@@ -1,5 +1,7 @@
 import numpy as np
-
+import json
+import requests
+import csv
 
 def get_exclusion_criteria_index(criteria):
     try:
@@ -11,6 +13,7 @@ def get_exclusion_criteria_index(criteria):
 
 def read_aact():
     # Sample AACT data read from here - https://www.ctti-clinicaltrials.org/aact-database
+    file_in = "/Users/charityhilton/Downloads/20181001_pipe-delimited-export/eligibilities.txt"
     with open('data/clinical_study.txt') as f:
         cols = None
         results = list()
@@ -45,5 +48,85 @@ def read_aact():
         return results
 
 
+def load_aact_in_solr():
+    file_in = "/Users/charityhilton/Downloads/20181001_pipe-delimited-export/eligibilities.txt"
+    solr_url = "http://18.220.133.76:8983/solr/sample"
+    url = solr_url + '/update?commit=true'
+    headers = {
+        'Content-type': 'application/json',
+    }
+
+    result_list = list()
+    with open(file_in) as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter='|')
+        line = 0
+        cols = []
+        for row in csv_reader:
+            if line == 0:
+                cols = row
+                print(cols)
+            else:
+                print(row[1])
+                criteria = str(row[8])
+                criteria = criteria.replace('~', '\n')
+                criteria = criteria.encode("ascii", errors="ignore").decode()
+                ex_index = get_exclusion_criteria_index(criteria)
+
+                if ex_index >= 0:
+                    inclusion = criteria[0:ex_index]
+                    exclusion = criteria[ex_index:]
+                else:
+                    inclusion = criteria
+                    exclusion = ''
+
+                d = {
+                        "subject": row[1],
+                         "description_attr": "AACT Clinical Trials",
+                         "source": "AACT",
+                         "report_date": "2018-10-24T00:00:00Z"
+                     }
+                for i in range(len(cols)):
+                    if i != 0 and i != 1 and i != 7 and i != 8:
+                        colname = cols[i] + "_attr"
+                        d[colname] = str(row[i])
+
+                d2 = d.copy()
+
+                d["report_type"] = "Clinical Trial Inclusion Criteria"
+                d["id"] = "ct_inc_" + row[0]
+                d["report_id"] = "ct_inc_" + row[0]
+                d["report_text"] = inclusion
+                result_list.append(d)
+
+                if len(exclusion) > 0:
+                    d2["report_type"] = "Clinical Trial Exclusion Criteria"
+                    d2["id"] = "ct_exc_" + row[0]
+                    d2["report_id"] = "ct_exc_" + row[0]
+                    d2["report_text"] = exclusion
+                    result_list.append(d2)
+                else:
+                    print('no exclusion')
+            line += 1
+
+            if line % 100 == 0:
+                print("parsed {0} lines".format(str(line)))
+                # Pushing data to Solr
+                data = json.dumps(result_list)
+                response2 = requests.post(url, headers=headers, data=data)
+
+                if response2.status_code == 200:
+                    print("Uploaded AACT eligibilities batch")
+                result_list = list()
+
+    print("parsed {0} lines".format(str(line)))
+    # Pushing data to Solr
+    data = json.dumps(result_list)
+    response2 = requests.post(url, headers=headers, data=data)
+
+    if response2.status_code == 200:
+        print("Uploaded AACT eligibilities all")
+
+
 if __name__ == "__main__":
-    read_aact()
+    # read_aact()
+    load_aact_in_solr()
