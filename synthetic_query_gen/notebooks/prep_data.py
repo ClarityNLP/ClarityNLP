c011108e029746519cb8782468cbe1eec1ae6c56
Charity Hilton <Charity.Hilton@gtri.gatech.edu>
2018-10-25 17:04:41 -0400

    add clarity features for aact data (race and gender still partial)

--- synthetic_query_gen/notebooks/prep_data.py
+++ synthetic_query_gen/notebooks/prep_data.py
@@ -3,6 +3,7 @@ import json
 import requests
 import csv
 
+
 def get_exclusion_criteria_index(criteria):
     try:
         ex_index = criteria.lower().index("exclusion criteria")
@@ -48,7 +49,7 @@ def read_aact():
         return results
 
 
-def load_aact_in_solr():
+def load_criteria_in_solr():
     file_in = "/Users/charityhilton/Downloads/20181001_pipe-delimited-export/eligibilities.txt"
     solr_url = "http://18.220.133.76:8983/solr/sample"
     url = solr_url + '/update?commit=true'
@@ -67,8 +68,23 @@ def load_aact_in_solr():
                 print(cols)
             else:
                 print(row[1])
-                criteria = str(row[8])
-                criteria = criteria.replace('~', '\n')
+                criteria_str = str(row[8])
+                criteria_split = criteria_str.split('~')
+                criteria = ''
+                n = 0
+                for i in criteria_split:
+                    stripped = i.strip()
+                    if len(stripped) > 0:
+                        if n == 0:
+                            criteria = stripped
+                        elif not (stripped[0].islower() or stripped[0].isnumeric() or stripped[0] == ')' or stripped[0] == '(')  and n > 0:
+                            criteria += '\n '
+                            criteria += stripped
+                        else:
+                            criteria += ' '
+                            criteria += stripped
+                    n += 1
+
                 criteria = criteria.encode("ascii", errors="ignore").decode()
                 ex_index = get_exclusion_criteria_index(criteria)
 
@@ -83,7 +99,7 @@ def load_aact_in_solr():
                         "subject": row[1],
                          "description_attr": "AACT Clinical Trials",
                          "source": "AACT",
-                         "report_date": "2018-10-24T00:00:00Z"
+                         "report_date": "2018-10-25T00:00:00Z"
                      }
                 for i in range(len(cols)):
                     if i != 0 and i != 1 and i != 7 and i != 8:
@@ -127,6 +143,189 @@ def load_aact_in_solr():
         print("Uploaded AACT eligibilities all")
 
 
+def load_descriptions_in_solr():
+    file_in = "/Users/charityhilton/Downloads/20181001_pipe-delimited-export/detailed_descriptions.txt"
+    solr_url = "http://18.220.133.76:8983/solr/sample"
+    url = solr_url + '/update?commit=true'
+    headers = {
+        'Content-type': 'application/json',
+    }
+
+    result_list = list()
+    with open(file_in) as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter='|')
+        line = 0
+        cols = []
+        for row in csv_reader:
+            if line == 0:
+                cols = row
+                print(cols)
+            else:
+                print(row[1])
+                txt_str = str(row[2])
+                txt_split = txt_str.split('~')
+                txt = ''
+                n = 0
+                for i in txt_split:
+                    stripped = i.strip()
+                    if len(stripped) > 0:
+                        if n == 0:
+                            txt = stripped
+                        elif not (stripped[0].islower() or stripped[0].isnumeric() or stripped[0] == ')' or stripped[0] == '(') and n > 0:
+                            txt += '\n '
+                            txt += stripped
+                        else:
+                            txt += ' '
+                            txt += stripped
+
+                    n += 1
+                d = {"subject": row[1],
+                     "description_attr": "AACT Clinical Trials",
+                     "source": "AACT",
+                     "report_type": "Clinical Trial Description",
+                     "report_text": txt,
+                     "report_id": row[0],
+                     "id": row[0],
+                     "report_date":  "2018-10-25T00:00:00Z"
+                     }
+                result_list.append(d)
+            line += 1
+
+            if line % 100 == 0:
+                print("parsed {0} lines".format(str(line)))
+                # Pushing data to Solr
+                data = json.dumps(result_list)
+                response2 = requests.post(url, headers=headers, data=data)
+
+                if response2.status_code == 200:
+                    print("Uploaded AACT description batch")
+                result_list = list()
+
+    print("parsed {0} lines".format(str(line)))
+    # Pushing data to Solr
+    data = json.dumps(result_list)
+    response2 = requests.post(url, headers=headers, data=data)
+
+    if response2.status_code == 200:
+        print("Uploaded AACT description all")
+
+
+def load_interventions_in_solr():
+    file_in = "/Users/charityhilton/Downloads/20181001_pipe-delimited-export/interventions.txt"
+    solr_url = "http://18.220.133.76:8983/solr/sample"
+    url = solr_url + '/update?commit=true'
+    headers = {
+        'Content-type': 'application/json',
+    }
+
+    result_list = list()
+    with open(file_in) as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter='|')
+        line = 0
+        cols = []
+        for row in csv_reader:
+            if line == 0:
+                cols = row
+                print(cols)
+            else:
+                print(row[1])
+                txt_str = str(row[4])
+                txt_split = txt_str.split('~')
+                txt = ''
+                n = 0
+                for i in txt_split:
+                    stripped = i.strip()
+                    if len(stripped) > 0:
+                        if n == 0:
+                            txt = stripped
+                        elif not (stripped[0].islower() or stripped[0].isnumeric() or stripped[0] == ')' or stripped[0] == '(') and n > 0:
+                            txt += '\n '
+                            txt += stripped
+                        else:
+                            txt += ' '
+                            txt += stripped
+                    n += 1
+                # id|nct_id|intervention_type|name|description
+                d = {"subject": row[1],
+                     "description_attr": "AACT Clinical Trials",
+                     "source": "AACT",
+                     "report_type": "Clinical Trial Interventions",
+                     "report_text": txt,
+                     "report_id": row[0],
+                     "id": row[0],
+                     "type_attr": str(row[2]),
+                     "name_attr": str(row[3]),
+                     "report_date":  "2018-10-25T00:00:00Z"
+                     }
+                result_list.append(d)
+            line += 1
+
+            if line % 100 == 0:
+                print("parsed {0} lines".format(str(line)))
+                # Pushing data to Solr
+                data = json.dumps(result_list)
+                response2 = requests.post(url, headers=headers, data=data)
+
+                if response2.status_code == 200:
+                    print("Uploaded AACT intervention batch")
+                result_list = list()
+
+    print("parsed {0} lines".format(str(line)))
+    # Pushing data to Solr
+    data = json.dumps(result_list)
+    response2 = requests.post(url, headers=headers, data=data)
+
+    if response2.status_code == 200:
+        print("Uploaded AACT intevention all")
+
+
+def write_all_inclusion_ids():
+    # Sample AACT data read from here - https://www.ctti-clinicaltrials.org/aact-database
+    file_in = "/Users/charityhilton/Downloads/20181001_pipe-delimited-export/eligibilities.txt"
+    with open(file_in) as f:
+        csv_reader = csv.reader(f, delimiter='|')
+        line = 0
+        cols = []
+        ids = open("data/nct_ids.txt","w+")
+        for row in csv_reader:
+            if line > 0:
+                id = row[1]
+
+                criteria_str = str(row[8])
+                criteria_split = criteria_str.split('~')
+                criteria = ''
+                n = 0
+                for i in criteria_split:
+                    stripped = i.strip()
+                    if len(stripped) > 0:
+                        if n == 0:
+                            criteria = stripped
+                        elif not (stripped[0].islower() or stripped[0].isnumeric() or stripped[0] == ')' or stripped[0] == '(')  and n > 0:
+                            criteria += '\n '
+                            criteria += stripped
+                        else:
+                            criteria += ' '
+                            criteria += stripped
+                    n += 1
+
+                criteria = criteria.encode("ascii", errors="ignore").decode()
+                ex_index = get_exclusion_criteria_index(criteria)
+
+                if ex_index >= 0:
+                    inclusion = criteria[0:ex_index]
+                else:
+                    inclusion = criteria
+                if len(inclusion) > 0:
+                    ids.write(id)
+                    ids.write('\n')
+            line += 1
+        ids.close()
+
+
 if __name__ == "__main__":
     # read_aact()
-    load_aact_in_solr()
+    # load_criteria_in_solr()
+    # load_descriptions_in_solr()
+    # load_interventions_in_solr()
+    write_all_inclusion_ids()
+    print('done')
