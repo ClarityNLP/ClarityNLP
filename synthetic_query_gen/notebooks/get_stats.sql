e1c6b7df540b721ab5a112154decd494625e38c8
Charity Hilton <Charity.Hilton@gtri.gatech.edu>
2018-11-29 16:19:49 -0500

    add stats query

---
+++ synthetic_query_gen/notebooks/get_stats.sql
@@ -0,0 +1,14 @@
+SELECT nj.nlp_job_id, nj.name, nj.spl[1] as job_type, nj.spl[3] as job_index, nj.status, nj.date_started, nj.date_ended,
+  c.description as result_feature_count,
+  d.description as result_patient_count,
+  a.description as luigi_workers,
+  b.description as batch_size,
+  e.description as memory_caching
+
+from (select *, regexp_split_to_array(name, '_') as spl from nlp.nlp_job) as nj
+left JOIN nlp.nlp_job_status a on nj.nlp_job_id = a.nlp_job_id and a.status = 'PROPERTIES_LUIGI_WORKERS'
+left JOIN nlp.nlp_job_status b on nj.nlp_job_id = b.nlp_job_id and b.status = 'PROPERTIES_BATCH_SIZE'
+left JOIN nlp.nlp_job_status c on nj.nlp_job_id = c.nlp_job_id and c.status = 'STATS_FINAL_RESULTS'
+left JOIN nlp.nlp_job_status d on nj.nlp_job_id = d.nlp_job_id and d.status = 'STATS_FINAL_SUBJECTS'
+left JOIN nlp.nlp_job_status e on nj.nlp_job_id = e.nlp_job_id and e.status = 'PROPERTIES_USE_MEMORY_CACHING'
+order by nj.nlp_job_id;
