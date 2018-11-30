e335e585ae8fbcebf2ecc95860a0fab7fcbb532b
Charity Hilton <Charity.Hilton@gtri.gatech.edu>
2018-11-29 22:36:47 -0500

    add support for failed job counts in query

--- synthetic_query_gen/notebooks/get_stats.sql
+++ synthetic_query_gen/notebooks/get_stats.sql
@@ -1,7 +1,7 @@
 SELECT nj.nlp_job_id, nj.name, nj.spl[1] as job_type, nj.spl[3] as job_index, nj.status, nj.date_started, nj.date_ended,
   case when nj.date_ended is not null then EXTRACT(MINUTE from (nj.date_ended - nj.date_started)) else 0 end as total_minutes,
-  c.description as result_feature_count,
-  d.description as result_patient_count,
+  case when c.description = '0' then f.description else c.description end as feature_count,
+  case when d.description  = '0' then  g.description else d.description end as patient_count,
   a.description as luigi_workers,
   b.description as batch_size,
   e.description as memory_caching
@@ -12,4 +12,6 @@ left JOIN nlp.nlp_job_status b on nj.nlp_job_id = b.nlp_job_id and b.status = 'P
 left JOIN nlp.nlp_job_status c on nj.nlp_job_id = c.nlp_job_id and c.status = 'STATS_FINAL_RESULTS'
 left JOIN nlp.nlp_job_status d on nj.nlp_job_id = d.nlp_job_id and d.status = 'STATS_FINAL_SUBJECTS'
 left JOIN nlp.nlp_job_status e on nj.nlp_job_id = e.nlp_job_id and e.status = 'PROPERTIES_USE_MEMORY_CACHING'
+left JOIN nlp.nlp_job_status f on nj.nlp_job_id = f.nlp_job_id and f.status = 'STATS_INTERMEDIATE_RESULTS'
+left JOIN nlp.nlp_job_status g on nj.nlp_job_id = g.nlp_job_id and g.status = 'STATS_INTERMEDIATE_SUBJECTS'
 order by nj.nlp_job_id;
