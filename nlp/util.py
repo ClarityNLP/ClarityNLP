768bdc9b2e2fc78efdc43215a63764f5169268d8
Charity Hilton <Charity.Hilton@gtri.gatech.edu>
2018-12-01 17:59:57 -0500

    tweak property loader

--- nlp/util.py
+++ nlp/util.py
@@ -1,11 +1,12 @@
 import configparser
 from os import getenv, environ, path
-import sys
 
 SCRIPT_DIR = path.dirname(__file__)
 config = configparser.RawConfigParser()
 config.read(path.join(SCRIPT_DIR, 'project.cfg'))
 properties = dict()
+delimiter = ','
+quote_character = '"'
 
 
 def read_property(env_name, config_tuple, default=''):
@@ -43,45 +44,31 @@ def read_boolean_property(prop, default=False):
 
 
 solr_url = read_property('NLP_SOLR_URL', ('solr', 'url'))
-
-conn_string = "host='%s' dbname='%s' user='%s' password='%s' port=%s" % (read_property('NLP_PG_HOSTNAME', ('pg', 'host')),
-                                                                         read_property('NLP_PG_DATABASE', ('pg', 'dbname')),
-                                                                         read_property('NLP_PG_USER', ('pg', 'user')),
-                                                                         read_property('NLP_PG_PASSWORD', ('pg', 'password')),
-                                                                         str(read_property('NLP_PG_CONTAINER_PORT', ('pg', 'port'))))
-
+conn_string = "host='%s' dbname='%s' user='%s' password='%s' port=%s" % (
+    read_property('NLP_PG_HOSTNAME', ('pg', 'host')),
+    read_property('NLP_PG_DATABASE', ('pg', 'dbname')),
+    read_property('NLP_PG_USER', ('pg', 'user')),
+    read_property('NLP_PG_PASSWORD', ('pg', 'password')),
+    str(read_property('NLP_PG_CONTAINER_PORT', ('pg', 'port'))))
 mongo_host = read_property('NLP_MONGO_HOSTNAME', ('mongo', 'host'))
 mongo_port = int(read_property('NLP_MONGO_CONTAINER_PORT', ('mongo', 'port')))
 mongo_db = read_property('NLP_MONGO_DATABASE', ('mongo', 'db'))
 mongo_working_index = read_property('NLP_MONGO_WORKING_INDEX', ('mongo', 'working_index'))
 mongo_working_collection = read_property('NLP_MONGO_WORKING_COLLECTION', ('mongo', 'working_collection'))
-
 tmp_dir = read_property('NLP_API_TMP_DIR', ('tmp', 'dir'))
 log_dir = read_property('NLP_API_LOG_DIR', ('log', 'dir'))
-
 luigi_scheduler = read_property('LUIGI_SCHEDULER_URL', ('luigi', 'scheduler'))
 luigi_url = read_property('LUIGI_URL', ('luigi', 'url'))
 luigi_workers = read_property('LUIGI_WORKERS', ('luigi', 'workers'))
-
 results_viewer_url = read_property('RESULTS_CLIENT_URL', ('results_client', 'url'))
-
 main_url = read_property('NLP_API_URL', ('main', 'url'))
-
-row_count = read_property('BATCH_SIZE', ('solr', 'batch_size'), default=25)
-
-delimiter = ','
-quote_character = '"'
-
+row_count = read_property('BATCH_SIZE', ('solr', 'batch_size'), default='10')
 report_mapper_url = read_property('MAPPER_API_URL', ('report_mapper', 'url'))
 report_mapper_key = read_property('MAPPER_API_KEY', ('report_mapper', 'key'))
 report_mapper_inst = read_property('MAPPER_API_INSTITUTE', ('report_mapper', 'institute'))
-
 ohdsi_url = read_property('OHDSI_WEBAPI_URL', ('ohdsi', 'webapi'))
-
 debug_mode = read_property('NLP_API_DEBUG_MODE', ('local', 'debug'))
-
 azure_key = read_property('NLP_AZURE_KEY', ('apis', 'azure_key'))
-
 solr_text_field = read_property('SOLR_TEXT_FIELD', ('solr', 'text_field'))
 solr_id_field = read_property('SOLR_ID_FIELD', ('solr', 'id_field'))
 solr_report_id_field = read_property('SOLR_REPORT_ID_FIELD', ('solr', 'report_id_field'))
@@ -89,14 +76,12 @@ solr_source_field = read_property('SOLR_SOURCE_FIELD', ('solr', 'source_field'))
 solr_report_date_field = read_property('SOLR_REPORT_DATE_FIELD', ('solr', 'date_field'))
 solr_subject_field = read_property('SOLR_SUBJECT_FIELD', ('solr', 'subject_field'))
 solr_report_type_field = read_property('SOLR_REPORT_TYPE_FIELD', ('solr', 'type_field'))
-
 expression_evaluator = read_property('NLP_EXPRESSION_EVALUATOR', ('local', 'evaluator'))
-
-use_memory_caching = read_boolean_property(read_property('USE_MEMORY_CACHING', ('optimizations', 'use_memory_cache'),
-                                                         default='true'))
-use_precomputed_segmentation = read_boolean_property(read_property('USE_PRECOMPUTED_SEGMENTATION',
-                                                                   ('optimizations', 'use_precomputed_segmentation'),
-                                                                   default='false'))
+use_memory_caching = read_property('USE_MEMORY_CACHING', ('optimizations', 'use_memory_cache'),
+                                   default='true')
+use_precomputed_segmentation = read_property('USE_PRECOMPUTED_SEGMENTATION',
+                                             ('optimizations', 'use_precomputed_segmentation'),
+                                             default='false')
 
 
 def cmp_2_key(mycmp):
@@ -107,4 +92,5 @@ def cmp_2_key(mycmp):
 
         def __cmp__(self, other):
             return mycmp(self.obj, other.obj)
+
     return K
