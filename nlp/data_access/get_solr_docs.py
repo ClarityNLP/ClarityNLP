2bcacfdd7fbfe3c65b9be1f843644e682ddee89a
Charity Hilton <Charity.Hilton@gtri.gatech.edu>
2018-12-03 13:32:56 -0500

    add script for getting solr text

---
+++ nlp/data_access/get_solr_docs.py
@@ -0,0 +1,72 @@
+import util
+from algorithms import segmentation, segmentation_init
+try:
+    from .solr_data import query, query_doc_size
+except Exception:
+    from solr_data import query, query_doc_size
+
+segment = segmentation.Segmentation()
+
+solr_url = "http://3.16.75.68:8983/solr/sample"
+headers = {
+    'Content-type': 'application/json',
+}
+batch_size = 10
+doc_size = 0
+spacy = segmentation_init()
+
+
+def document_sentences(txt):
+    sentence_list = segment.parse_sentences(txt, spacy=spacy)
+    return sentence_list
+
+
+def document_text(doc, clean=True):
+    if doc and util.solr_text_field in doc:
+        txt = doc[util.solr_text_field]
+        if type(txt) == str:
+            txt_val = txt
+        elif type(txt) == list:
+            txt_val = ' '.join(txt)
+        else:
+            txt_val = str(txt)
+
+        if clean:
+            return txt_val.encode("ascii", errors="ignore").decode()
+        else:
+            return txt_val
+    else:
+        return ''
+
+
+def do_something(q, n):
+    try:
+        docs = query(q, solr_url=solr_url, mapper_inst=util.report_mapper_inst,
+                     mapper_key=util.report_mapper_key,
+                     mapper_url=util.report_mapper_url, start=n, rows=batch_size)
+        for doc in docs:
+            txt = document_text(doc, clean=True)
+            # do something
+    except Exception as ex:
+        print('exception getting docs')
+        return False
+    return True
+
+
+def get_documents(q):
+    global doc_size
+    doc_size = query_doc_size(q, solr_url=solr_url, mapper_inst=util.report_mapper_inst,
+                              mapper_key=util.report_mapper_key, mapper_url=util.report_mapper_url)
+
+    i = 0
+    while i < doc_size:
+        print("on batch %d" % i)
+        do_something(q, i)
+
+        i += batch_size
+
+
+if __name__ == "__main__":
+    print('get_solr_docs')
+    qry = "*:*"
+    get_documents(qry)
