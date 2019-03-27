c7dbc59ef38e2808f21a44028fc7fbb0d20d6698
Caleb Sides <calebsides@LPATLICLLOAN13M.local>
2019-03-27 10:47:31 -0400

    added endpoints to proxy

--- identity-and-access-proxy/proxy.js
+++ identity-and-access-proxy/proxy.js
@@ -1,144 +1,173 @@
-const http = require('http');
-const httpProxy = require('http-proxy');
-const HttpProxyRules = require('http-proxy-rules');
-const request = require('request');
-const jwksClient = require('jwks-rsa');
-var jwt = require('jsonwebtoken');
-const url = require('url');
+const http = require("http");
+const httpProxy = require("http-proxy");
+const HttpProxyRules = require("http-proxy-rules");
+const request = require("request");
+const jwksClient = require("jwks-rsa");
+var jwt = require("jsonwebtoken");
+const url = require("url");
 
 const allowedOrigins = [
-  'dashboard.clarity.localhost',
-  'ingest.clarity.localhost',
-  'viewer.clarity.localhost' ,
-  'http://dashboard.clarity.localhost',
-  'http://ingest.clarity.localhost',
-  'http://viewer.clarity.localhost',
-  'localhost',
+    "dashboard.clarity.localhost",
+    "ingest.clarity.localhost",
+    "viewer.clarity.localhost",
+    "http://dashboard.clarity.localhost",
+    "http://ingest.clarity.localhost",
+    "http://viewer.clarity.localhost",
+    "localhost"
 ];
 
 // Set up proxy rules instance
 var proxyRules = new HttpProxyRules({
-  rules: {
-    // NLP-API
-    '/api/nlp/pipeline_types': 'http://nlp-api:5000/pipeline_types', //NLP-API --> GET pipeline_types
-    '/api/nlp/phenotype_jobs/ALL': 'http://nlp-api:5000/phenotype_jobs/ALL',
-    '/api/nlp/phenotype_subjects/(.+)/(.+)': 'http://nlp-api:5000/phenotype_subjects/$1/$2',
-    '/api/nlp/phenotype_subject_results/(.+)/(.+)/(.+)': 'http://nlp-api:5000/phenotype_subject_results/$1/$2/$3',
-    '/api/nlp/phenotype_job_by_id/(.+)': 'http://nlp-api:5000/phenotype_job_by_id/$1',
-    '/api/nlp/phenotype_paged_results/(.+)/(.+)': 'http://nlp-api:5000/phenotype_paged_results/$1/$2',
-    '/api/nlp/export_ohdsi': 'http://nlp-api:5000/export_ohdsi',
-    '/api/nlp/nlpql_tester': 'http://nlp-api:5000/nlpql_tester',
-    '/api/nlp/add_query': 'http://nlp-api:5000/add_query',
-    '/api/nlp/nlpql': 'http://nlp-api:5000/nlpql',
-    // INGEST-API
-    '/api/ingest/fields': 'http://ingest-api:1337/fields', //INGEST-API --> GET fields
-    '/api/ingest/core': 'http://ingest-api:1337/solr/core', //INGEST-API --> GET solr/core
-    '/api/ingest/numDocs': 'http://ingest-api:1337/solr/numDocs', //INGEST-API --> GET solr/numDocs
-    '/api/ingest/csv': 'http://ingest-api:1337/ingest/csv', //INGEST-API --> POST /ingest/csv
-    '/api/ingest/upload': 'http://ingest-api:1337/upload', //INGEST-API --> POST /upload
-    '/api/ingest/(.+)/schedule': 'http://ingest-api:1337/ingest/$1/schedule', //INGEST-API --> POST /ingest/:ingestId/schedule
-    '/api/ingest': 'http://ingest-api:1337/ingest', //INGEST-API --> GET /ingest
-    '/api/ingest/(.+)/delete': 'http://ingest-api:1337/ingest/$1/delete', //INGEST-API --> GET /ingest/:ingestId/delete
-    // DASHBOARD-API
-    '/api/dashboard': 'ws://dashboard-api:8750' //DASHBOARD-API --> websocket connection
-  }
+    rules: {
+        // NLP-API
+        "/api/nlp/pipeline_types": "http://nlp-api:5000/pipeline_types", //NLP-API --> GET pipeline_types
+        "/api/nlp/phenotype_jobs/ALL": "http://nlp-api:5000/phenotype_jobs/ALL",
+        "/api/nlp/phenotype_subjects/(.+)/(.+)":
+            "http://nlp-api:5000/phenotype_subjects/$1/$2",
+        "/api/nlp/phenotype_subject_results/(.+)/(.+)/(.+)":
+            "http://nlp-api:5000/phenotype_subject_results/$1/$2/$3",
+        "/api/nlp/phenotype_job_by_id/(.+)":
+            "http://nlp-api:5000/phenotype_job_by_id/$1",
+        "/api/nlp/phenotype_paged_results/(.+)/(.+)":
+            "http://nlp-api:5000/phenotype_paged_results/$1/$2",
+        "/api/nlp/export_ohdsi": "http://nlp-api:5000/export_ohdsi",
+        "/api/nlp/nlpql_tester": "http://nlp-api:5000/nlpql_tester",
+        "/api/nlp/add_query": "http://nlp-api:5000/add_query",
+        "/api/nlp/nlpql": "http://nlp-api:5000/nlpql",
+        "/api/nlp/phenotype_results_by_id/(.+)":
+            "http://nlp-api:5000/phenotype_results_by_id/$1",
+        // INGEST-API
+        "/api/ingest/fields": "http://ingest-api:1337/fields", //INGEST-API --> GET fields
+        "/api/ingest/core": "http://ingest-api:1337/solr/core", //INGEST-API --> GET solr/core
+        "/api/ingest/numDocs": "http://ingest-api:1337/solr/numDocs", //INGEST-API --> GET solr/numDocs
+        "/api/ingest/csv": "http://ingest-api:1337/ingest/csv", //INGEST-API --> POST /ingest/csv
+        "/api/ingest/upload": "http://ingest-api:1337/upload", //INGEST-API --> POST /upload
+        "/api/ingest/(.+)/schedule":
+            "http://ingest-api:1337/ingest/$1/schedule", //INGEST-API --> POST /ingest/:ingestId/schedule
+        "/api/ingest": "http://ingest-api:1337/ingest", //INGEST-API --> GET /ingest
+        "/api/ingest/(.+)/delete": "http://ingest-api:1337/ingest/$1/delete", //INGEST-API --> GET /ingest/:ingestId/delete
+        // DASHBOARD-API
+        "/api/dashboard": "ws://dashboard-api:8750" //DASHBOARD-API --> websocket connection
+    }
 });
 
 // Create reverse proxy instance
 var proxy = httpProxy.createProxyServer();
 
 var sendError = function(res, err) {
-  res.writeHead(500, { 'Content-Type': 'application/json' });
-  return res.end(err);
+    res.writeHead(500, { "Content-Type": "application/json" });
+    return res.end(err);
 };
 
 // error handling
-proxy.on("error", function (err, req, res) {
-	sendError(res, err);
+proxy.on("error", function(err, req, res) {
+    sendError(res, err);
 });
 
 var enableCors = function(req, res) {
-	if (req.headers['access-control-request-method']) {
-		res.setHeader('access-control-allow-methods', req.headers['access-control-request-method']);
-	}
+    if (req.headers["access-control-request-method"]) {
+        res.setHeader(
+            "access-control-allow-methods",
+            req.headers["access-control-request-method"]
+        );
+    }
 
-	if (req.headers['access-control-request-headers']) {
-		res.setHeader('access-control-allow-headers', req.headers['access-control-request-headers']);
-	}
+    if (req.headers["access-control-request-headers"]) {
+        res.setHeader(
+            "access-control-allow-headers",
+            req.headers["access-control-request-headers"]
+        );
+    }
 
-	if (req.headers.origin) {
-		res.setHeader('access-control-allow-origin', req.headers.origin);
-		res.setHeader('access-control-allow-credentials', 'true');
-	}
+    if (req.headers.origin) {
+        res.setHeader("access-control-allow-origin", req.headers.origin);
+        res.setHeader("access-control-allow-credentials", "true");
+    }
 };
 
 // set header for CORS
 proxy.on("proxyRes", function(proxyRes, req, res) {
-	enableCors(req, res);
+    enableCors(req, res);
 });
 
 // Create http server that leverages reverse proxy instance
 // and proxy rules to proxy requests to different targets
 const proxyServer = http.createServer(function(req, res) {
+    if (req.method === "OPTIONS") {
+        enableCors(req, res);
+        res.writeHead(200);
+        res.end();
+        return;
+    }
 
-  if (req.method === 'OPTIONS') {
-		enableCors(req, res);
-		res.writeHead(200);
-		res.end();
-		return;
-	}
-
-  var client = jwksClient({
-    jwksUri: 'http://identity-provider:5000/.well-known/openid-configuration/jwks'
-  });
-  function getKey(header, callback){
-    client.getSigningKey(header.kid, function(err, key) {
-      if (err) {
-        return callback(err);
-      }
-      var signingKey = key.publicKey || key.rsaPublicKey;
-      callback(null, signingKey);
+    var client = jwksClient({
+        jwksUri:
+            "http://identity-provider:5000/.well-known/openid-configuration/jwks"
     });
-  }
-
-  let token;
-
-  try {
-    token = req.headers.authorization.substring(7, req.headers.authorization.length);
-  } catch {
-    res.writeHead(400, { 'Content-Type': 'text/plain' });
-    return res.end('Malformatted bearer token.');
-  }
-
-  jwt.verify(token, getKey, function(err, decoded) {
-    if (err) {
-      console.error(err);
-      res.writeHead(401, { 'Content-Type': 'text/plain' });
-      return res.end();
+    function getKey(header, callback) {
+        client.getSigningKey(header.kid, function(err, key) {
+            if (err) {
+                return callback(err);
+            }
+            var signingKey = key.publicKey || key.rsaPublicKey;
+            callback(null, signingKey);
+        });
     }
 
-    const target = proxyRules.match(req);
+    let token;
 
-    if (target) {
-      return proxy.web(req, res, {
-        target: target
-      }, function(err) {
-        sendError(res, err);
-      });
+    try {
+        token = req.headers.authorization.substring(
+            7,
+            req.headers.authorization.length
+        );
+    } catch {
+        res.writeHead(400, { "Content-Type": "text/plain" });
+        return res.end("Malformatted bearer token.");
     }
 
-    res.writeHead(400, { 'Content-Type': 'text/plain' });
-    return res.end('The request url and path did not match any of the listed rules.');
-  });
+    jwt.verify(token, getKey, function(err, decoded) {
+        if (err) {
+            console.error(err);
+            res.writeHead(401, { "Content-Type": "text/plain" });
+            return res.end();
+        }
+
+        const target = proxyRules.match(req);
+
+        if (target) {
+            return proxy.web(
+                req,
+                res,
+                {
+                    target: target
+                },
+                function(err) {
+                    sendError(res, err);
+                }
+            );
+        }
+
+        res.writeHead(400, { "Content-Type": "text/plain" });
+        return res.end(
+            "The request url and path did not match any of the listed rules."
+        );
+    });
 });
 
-proxyServer.on('upgrade', function (req, socket, head) {
-  proxy.ws(req, socket, head, {
-    target: 'ws://dashboard-api:8750'
-  }, function(err) {
-    console.log('err: ',err);
-    // sendError(res, err);
-  });
+proxyServer.on("upgrade", function(req, socket, head) {
+    proxy.ws(
+        req,
+        socket,
+        head,
+        {
+            target: "ws://dashboard-api:8750"
+        },
+        function(err) {
+            console.log("err: ", err);
+            // sendError(res, err);
+        }
+    );
 });
 
 proxyServer.listen(6010);
