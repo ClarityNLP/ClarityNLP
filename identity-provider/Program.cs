a39f08c510bc1c55af0d93264a862316a71febb5
Trey Schneider <trey@codeschneider.com>
2019-04-22 20:31:04 -0400

    remove write log to txt file

--- identity-provider/Program.cs
+++ identity-provider/Program.cs
@@ -27,12 +27,11 @@ namespace IdentityServer
                     .UseSerilog((context, configuration) =>
                     {
                         configuration
-                            .MinimumLevel.Verbose()
+                            .MinimumLevel.Debug()
                             .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
                             .MinimumLevel.Override("System", LogEventLevel.Warning)
                             .MinimumLevel.Override("Microsoft.AspNetCore.Authentication", LogEventLevel.Information)
                             .Enrich.FromLogContext()
-                            .WriteTo.File(@"identityserver4_log.txt")
                             .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level}] {SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}{NewLine}", theme: AnsiConsoleTheme.Literate);
                     });
         }
