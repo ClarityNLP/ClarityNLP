3aebd4eec7e0253774125b84de0df5a9373395d4
Trey Schneider <trey@codeschneider.com>
2019-04-22 20:28:53 -0400

    idp Config.cs env var driven, instantiate db

--- identity-provider/Config.cs
+++ identity-provider/Config.cs
@@ -7,6 +7,8 @@ using IdentityServer4.Models;
 using IdentityServer4.Test;
 using System.Collections.Generic;
 using System.Security.Claims;
+using Microsoft.Extensions.Configuration;
+using IdentityServer4.Configuration;
 
 namespace IdentityServer
 {
@@ -19,13 +21,12 @@ namespace IdentityServer
                 new TestUser
                 {
                     SubjectId = "1",
-                    Username = "admin",
-                    Password = "admin",
+                    Username = $"{Startup.StaticConfig["ADMIN_USERNAME"]}",
+                    Password = $"{Startup.StaticConfig["ADMIN_PASSWORD"]}",
 
                     Claims = new []
                     {
                         new Claim("name", "Admin"),
-                        new Claim("website", "https://www.hdap.gatech.edu")
                     }
                 }
             };
@@ -48,10 +49,6 @@ namespace IdentityServer
               {
                 Name = "ingest_api",
                 DisplayName = "Ingest API",
-                ApiSecrets =
-                {
-                    new Secret("secret".Sha256())
-                },
                 Scopes =
                 {
                     new Scope()
@@ -65,10 +62,6 @@ namespace IdentityServer
               {
                 Name = "solr_api",
                 DisplayName = "Solr API",
-                ApiSecrets =
-                {
-                    new Secret("secret".Sha256())
-                },
                 Scopes =
                 {
                     new Scope()
@@ -82,10 +75,6 @@ namespace IdentityServer
               {
                 Name = "nlp_api",
                 DisplayName = "NLP API",
-                ApiSecrets =
-                {
-                    new Secret("secret".Sha256())
-                },
                 Scopes =
                 {
                     new Scope()
@@ -104,67 +93,41 @@ namespace IdentityServer
             {
                 new Client
                 {
-                    ClientId = "client",
-
-                    // no interactive user, use the clientid/secret for authentication
+                    ClientId = "cli",
                     AllowedGrantTypes = GrantTypes.ClientCredentials,
-
-                    // secret for authentication
                     ClientSecrets =
                     {
-                        new Secret("secret".Sha256())
+                        new Secret(Startup.StaticConfig["CLIENT_CLI_SECRET"].Sha256())
                     },
-
-                    // scopes that client has access to
-                    AllowedScopes = { "api1" }
-                },
-                // resource owner password grant client
-                new Client
-                {
-                    ClientId = "ro.client",
-                    AllowedGrantTypes = GrantTypes.ResourceOwnerPassword,
-
-                    ClientSecrets =
-                    {
-                        new Secret("secret".Sha256())
-                    },
-                    AllowedScopes = { "api1" }
-                },
-                // OpenID Connect hybrid flow client (MVC)
-                new Client
-                {
-                    ClientId = "mvc",
-                    ClientName = "MVC Client",
-                    AllowedGrantTypes = GrantTypes.Hybrid,
-
-                    ClientSecrets =
-                    {
-                        new Secret("secret".Sha256())
-                    },
-
-                    RedirectUris           = { "http://localhost:5002/signin-oidc" },
-                    PostLogoutRedirectUris = { "http://localhost:5002/signout-callback-oidc" },
-
+                    AccessTokenLifetime = 3000000,
                     AllowedScopes =
                     {
-                        IdentityServerConstants.StandardScopes.OpenId,
-                        IdentityServerConstants.StandardScopes.Profile,
-                        "api1"
-                    },
-
-                    AllowOfflineAccess = true
+                        "ingest_api",
+                        "solr_api",
+                        "nlp_api"
+                    }
                 },
-                // SPA - Ingest Client
                 new Client
                 {
                     ClientId = "ingest",
                     ClientName = "Ingest",
+                    RequireConsent = false,
                     AllowedGrantTypes = GrantTypes.Code,
                     RequirePkce = true,
                     RequireClientSecret = false,
-                    RedirectUris =           { "https://ingest.claritynlp.dev/silent_renew.html", "https://ingest.claritynlp.dev/callback.html" },
-                    PostLogoutRedirectUris = { "https://ingest.claritynlp.dev/csv" },
-                    AllowedCorsOrigins =     { "https://ingest.claritynlp.dev" },
+                    RedirectUris =
+                    {
+                        $"{Startup.StaticConfig["PROTOCOL"]}://{Startup.StaticConfig["INGEST_SUBDOMAIN"]}.{Startup.StaticConfig["DOMAIN"]}/silent_renew.html",
+                        $"{Startup.StaticConfig["PROTOCOL"]}://{Startup.StaticConfig["INGEST_SUBDOMAIN"]}.{Startup.StaticConfig["DOMAIN"]}/callback.html"
+                    },
+                    PostLogoutRedirectUris =
+                    {
+                        $"{Startup.StaticConfig["PROTOCOL"]}://{Startup.StaticConfig["INGEST_SUBDOMAIN"]}.{Startup.StaticConfig["DOMAIN"]}/csv"
+                    },
+                    AllowedCorsOrigins =
+                    {
+                        $"{Startup.StaticConfig["PROTOCOL"]}://{Startup.StaticConfig["INGEST_SUBDOMAIN"]}.{Startup.StaticConfig["DOMAIN"]}"
+                    },
                     AllowedScopes =
                     {
                         IdentityServerConstants.StandardScopes.OpenId,
@@ -173,17 +136,27 @@ namespace IdentityServer
                         "solr_api"
                     }
                 },
-                // SPA - Results Viewer Client
                 new Client
                 {
                     ClientId = "viewer",
                     ClientName = "Results Viewer",
+                    RequireConsent = false,
                     AllowedGrantTypes = GrantTypes.Code,
                     RequirePkce = true,
                     RequireClientSecret = false,
-                    RedirectUris =           { "https://viewer.claritynlp.dev/silent_renew.html", "https://viewer.claritynlp.dev/callback.html" },
-                    PostLogoutRedirectUris = { "https://viewer.claritynlp.dev" },
-                    AllowedCorsOrigins =     { "https://viewer.claritynlp.dev" },
+                    RedirectUris =
+                    {
+                        $"{Startup.StaticConfig["PROTOCOL"]}://{Startup.StaticConfig["RESULTS_SUBDOMAIN"]}.{Startup.StaticConfig["DOMAIN"]}/silent_renew.html",
+                        $"{Startup.StaticConfig["PROTOCOL"]}://{Startup.StaticConfig["RESULTS_SUBDOMAIN"]}.{Startup.StaticConfig["DOMAIN"]}/callback.html"
+                    },
+                    PostLogoutRedirectUris =
+                    {
+                        $"{Startup.StaticConfig["PROTOCOL"]}://{Startup.StaticConfig["RESULTS_SUBDOMAIN"]}.{Startup.StaticConfig["DOMAIN"]}"
+                    },
+                    AllowedCorsOrigins =
+                    {
+                        $"{Startup.StaticConfig["PROTOCOL"]}://{Startup.StaticConfig["RESULTS_SUBDOMAIN"]}.{Startup.StaticConfig["DOMAIN"]}"
+                    },
                     AllowedScopes =
                     {
                         IdentityServerConstants.StandardScopes.OpenId,
@@ -191,17 +164,27 @@ namespace IdentityServer
                         "nlp_api"
                     }
                 },
-                // SPA - Dashboard Client
                 new Client
                 {
                     ClientId = "dashboard",
                     ClientName = "Dashboard",
+                    RequireConsent = false,
                     AllowedGrantTypes = GrantTypes.Code,
                     RequirePkce = true,
                     RequireClientSecret = false,
-                    RedirectUris =           { "https://dashboard.claritynlp.dev/silent_renew.html", "https://dashboard.claritynlp.dev/callback.html" },
-                    PostLogoutRedirectUris = { "https://dashboard.claritynlp.dev" },
-                    AllowedCorsOrigins =     { "https://dashboard.claritynlp.dev" },
+                    RedirectUris =
+                    {
+                        $"{Startup.StaticConfig["PROTOCOL"]}://{Startup.StaticConfig["DASHBOARD_SUBDOMAIN"]}.{Startup.StaticConfig["DOMAIN"]}/silent_renew.html",
+                        $"{Startup.StaticConfig["PROTOCOL"]}://{Startup.StaticConfig["DASHBOARD_SUBDOMAIN"]}.{Startup.StaticConfig["DOMAIN"]}/callback.html"
+                    },
+                    PostLogoutRedirectUris =
+                    {
+                        $"{Startup.StaticConfig["PROTOCOL"]}://{Startup.StaticConfig["DASHBOARD_SUBDOMAIN"]}.{Startup.StaticConfig["DOMAIN"]}"
+                    },
+                    AllowedCorsOrigins =
+                    {
+                        $"{Startup.StaticConfig["PROTOCOL"]}://{Startup.StaticConfig["DASHBOARD_SUBDOMAIN"]}.{Startup.StaticConfig["DOMAIN"]}"
+                    },
                     AllowedScopes =
                     {
                         IdentityServerConstants.StandardScopes.OpenId,
@@ -209,86 +192,6 @@ namespace IdentityServer
                         "nlp_api",
                         "solr_api"
                     }
-                },
-                // SPA - Redux OIDC Example App
-                new Client
-                {
-                    ClientId = "redux_test",
-                    ClientName = "Redux Test Client",
-                    AllowedGrantTypes = GrantTypes.Code,
-                    RequirePkce = true,
-                    RequireClientSecret = false,
-
-                    // RedirectUris =           { "http://localhost:5003/callback.html", "http://localhost:5003/silent.html" },
-                    RedirectUris =           { "http://localhost:9090/silent_renew.html", "http://localhost:9090/callback" },
-                    PostLogoutRedirectUris = { "http://localhost:9090" },
-                    AllowedCorsOrigins =     { "http://localhost:9090" },
-
-                    AllowedScopes =
-                    {
-                        IdentityServerConstants.StandardScopes.OpenId,
-                        IdentityServerConstants.StandardScopes.Profile,
-                        "api1"
-                    }
-                },
-                // JavaScript Client #1
-                new Client
-                {
-                    ClientId = "js",
-                    ClientName = "JavaScript Client",
-                    AllowedGrantTypes = GrantTypes.Code,
-                    RequirePkce = true,
-                    RequireClientSecret = false,
-
-                    // RedirectUris =           { "http://localhost:5003/callback.html", "http://localhost:5003/silent.html" },
-                    RedirectUris =           { "http://localhost:5003/silent_renew.html", "http://localhost:5003/callback" },
-                    PostLogoutRedirectUris = { "http://localhost:5003/index.html" },
-                    AllowedCorsOrigins =     { "http://localhost:5003" },
-
-                    AllowedScopes =
-                    {
-                        IdentityServerConstants.StandardScopes.OpenId,
-                        IdentityServerConstants.StandardScopes.Profile,
-                        "api1"
-                    }
-                },
-                // JavaScript Client #2
-                new Client
-                {
-                    ClientId = "js2",
-                    ClientName = "JavaScript Client",
-                    AllowedGrantTypes = GrantTypes.Code,
-                    RequirePkce = true,
-                    RequireClientSecret = false,
-
-                    RedirectUris =           { "http://localhost:5004/callback.html", "http://localhost:5004/silent.html" },
-                    // RedirectUris =           { "http://localhost:5003/callback" },
-                    PostLogoutRedirectUris = { "http://localhost:5004/index.html" },
-                    AllowedCorsOrigins =     { "http://localhost:5004" },
-
-                    AllowedScopes =
-                    {
-                        IdentityServerConstants.StandardScopes.OpenId,
-                        IdentityServerConstants.StandardScopes.Profile,
-                        "api1"
-                    }
-                },
-                // ORY Oathkeeper (Client Credentials)
-                new Client
-                {
-                    ClientId = "api1",
-
-                    // no interactive user, use the clientid/secret for authentication
-                    AllowedGrantTypes = GrantTypes.ClientCredentials,
-
-                    // secret for authentication
-                    ClientSecrets =
-                    {
-                        new Secret("secret".Sha256())
-                    },
-
-                    // scopes that client has access to
-                    // AllowedScopes = { "api1" }
                 }
             };
         }
