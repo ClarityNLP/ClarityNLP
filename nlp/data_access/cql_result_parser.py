50ed68b310ea238688033fa1d5305bbc2a72b4ef
Richard Boyd <richard.boyd@gtri.gatech.edu>
2019-09-05 09:37:30 -0400

    added decoding of MedicationStatement CQL Engine result

--- nlp/data_access/cql_result_parser.py
+++ nlp/data_access/cql_result_parser.py
@@ -1,57 +1,66 @@
 #!/usr/bin/env python3
 """
-Import-only module used to decode JSON results from the FHIR CQL wrapper.
+Module used to decode JSON results from the FHIR CQL wrapper.
 """
 
 import re
 import os
 import sys
 import json
-import optparse
+import argparse
 from datetime import datetime, timezone
 from collections import namedtuple
 
 _VERSION_MAJOR = 0
-_VERSION_MINOR = 2
+_VERSION_MINOR = 3
 _MODULE_NAME   = 'cibmtr_data_parser.py'
 
 # set to True to enable debug output
 _TRACE = False
 
 # dict keys used to extract portions of the JSON data
-_KEY_ABATEMENT_DATE_TIME = 'abatementDateTime'
-_KEY_CATEGORY            = 'category'
-_KEY_CODE                = 'code'
-_KEY_CODING              = 'coding'
-_KEY_CONTEXT             = 'context'
-_KEY_DISPLAY             = 'display'
-_KEY_DOB                 = 'birthDate'
-_KEY_EFF_DATE_TIME       = 'effectiveDateTime'
-_KEY_FAMILY_NAME         = 'family'
-_KEY_GENDER              = 'gender'
-_KEY_GIVEN_NAME          = 'given'
-_KEY_ID                  = 'id'
-_KEY_LOCATION            = 'location'
-_KEY_NAME                = 'name'
-_KEY_ONSET_DATE_TIME     = 'onsetDateTime'
-_KEY_PERFORMED_DATE_TIME = 'performedDateTime'
-_KEY_REFERENCE           = 'reference'
-_KEY_RESOURCE_TYPE       = 'resourceType'
-_KEY_RESULT              = 'result'
-_KEY_RESULT_TYPE         = 'resultType'
-_KEY_STATUS              = 'status'
-_KEY_SUBJECT             = 'subject'
-_KEY_SYSTEM              = 'system'
-_KEY_UNIT                = 'unit'
-_KEY_VALUE               = 'value'
-_KEY_VALUE_QUANTITY      = 'valueQuantity'
+_KEY_ABATEMENT_DATE_TIME  = 'abatementDateTime'
+_KEY_CATEGORY             = 'category'
+_KEY_CODE                 = 'code'
+_KEY_CODING               = 'coding'
+_KEY_CONTEXT              = 'context'
+_KEY_DISPLAY              = 'display'
+_KEY_DOB                  = 'birthDate'
+_KEY_DOSAGE               = 'dosage'
+_KEY_DOSE_QUANTITY        = 'doseQuantity'
+_KEY_EFF_DATE_TIME        = 'effectiveDateTime'
+_KEY_EFF_PERIOD           = 'effectivePeriod'
+_KEY_END                  = 'end'
+_KEY_FAMILY_NAME          = 'family'
+_KEY_GENDER               = 'gender'
+_KEY_GIVEN_NAME           = 'given'
+_KEY_ID                   = 'id'
+_KEY_LOCATION             = 'location'
+_KEY_MED_CODEABLE_CONCEPT = 'medicationCodeableConcept'
+_KEY_NAME                 = 'name'
+_KEY_ONSET_DATE_TIME      = 'onsetDateTime'
+_KEY_PERFORMED_DATE_TIME  = 'performedDateTime'
+_KEY_REFERENCE            = 'reference'
+_KEY_RESOURCE_TYPE        = 'resourceType'
+_KEY_RESULT               = 'result'
+_KEY_RESULT_TYPE          = 'resultType'
+_KEY_START                = 'start'
+_KEY_STATUS               = 'status'
+_KEY_SUBJECT              = 'subject'
+_KEY_SYSTEM               = 'system'
+_KEY_TAKEN                = 'taken'
+_KEY_UNIT                 = 'unit'
+_KEY_VALUE                = 'value'
+_KEY_VALUE_QUANTITY       = 'valueQuantity'
 
-_STR_BUNDLE      = 'FhirBundleCursorStu2'
-#_STR_CONCEPT     = 'Concept'
-_STR_CONDITION   = 'Condition'
-_STR_OBSERVATION = 'Observation'
-_STR_PATIENT     = 'Patient'
-_STR_PROCEDURE   = 'Procedure'
+#_STR_CONCEPT             = 'Concept'
+_STR_BUNDLE2              = 'FhirBundleCursorStu2'
+_STR_BUNDLE3              = 'FhirBundleCursorStu3'
+_STR_CONDITION            = 'Condition'
+_STR_OBSERVATION          = 'Observation'
+_STR_PATIENT              = 'Patient'
+_STR_PROCEDURE            = 'Procedure'
+_STR_MEDICATION_STATEMENT = 'MedicationStatement'
 
 # fields extracted from a 'Patient' FHIR resource
 PATIENT_FIELDS = [
@@ -107,6 +116,24 @@ ObservationResource = namedtuple('ObservationResource', OBSERVATION_FIELDS)
 
 CODING_FIELDS = ['code', 'system', 'display']
 CodingObj = namedtuple('CodingObj', CODING_FIELDS)
+
+DOSE_QUANTITY_FIELDS = ['value', 'unit', 'system', 'code']
+DoseQuantityObj = namedtuple('DoseQuantityObj', DOSE_QUANTITY_FIELDS)
+
+# fields extracted from a 'MedicationStatement' FHIR resource
+MEDICATION_STATEMENT_FIELDS = [
+    'id_value',
+    'context_reference',
+    'medication_codeable_concept',
+    'subject_reference',
+    'subject_display',
+    'taken',         # Boolean
+    'dosage_list',   # list of DoseQuantity objects
+    'date_time',
+    'end_date_time'
+]
+MedicationStatementResource = namedtuple('MedicationStatementResource',
+                                         MEDICATION_STATEMENT_FIELDS)
 
 # regex used to recognize UTC offsets in a FHIR datetime string
 _regex_fhir_utc_offset = re.compile(r'\+\d\d:\d\d\Z')
@@ -169,8 +196,12 @@ def _decode_code_dict(obj):
     """
 
     coding_systems_list = []
+    code_dict = None
     if _KEY_CODE in obj:
         code_dict = obj[_KEY_CODE]
+    elif _KEY_MED_CODEABLE_CONCEPT in obj:
+        code_dict = obj[_KEY_MED_CODEABLE_CONCEPT]
+    if code_dict is not None:
         # should have a 'coding' key
         if _KEY_CODING in code_dict:
             # value should be a list
@@ -240,6 +271,98 @@ def _decode_id_value(obj):
         id_value = obj[_KEY_ID]
 
     return id_value
+
+
+###############################################################################
+def _decode_effective_period(obj):
+
+    if _KEY_START in obj:
+        start_date_time = obj[_KEY_START]
+        start_date_time = _fixup_fhir_datetime(start_date_time)
+        start_date_time = datetime.strptime(start_date_time, '%Y-%m-%dT%H:%M:%S%z')
+    if _KEY_END in obj:
+        end_date_time = obj[_KEY_END]
+        end_date_time = _fixup_fhir_datetime(end_date_time)
+        end_date_time = datetime.strptime(end_date_time, '%Y-%m-%dT%H:%M:%S%z')
+        
+    return (start_date_time, end_date_time)
+
+
+###############################################################################
+def _decode_dosage(obj):
+
+    dosage_list = []
+    
+    if _KEY_DOSAGE in obj:
+        dl = obj[_KEY_DOSAGE]
+        assert list == type(dl)
+        for elt in dl:
+            dq_obj = None
+            if _KEY_DOSE_QUANTITY in elt:
+                dq = elt[_KEY_DOSE_QUANTITY]
+                assert dict == type(dq)
+                value = None
+                if _KEY_VALUE in dq:
+                    value = dq[_KEY_VALUE]
+                unit = None
+                if _KEY_UNIT in dq:
+                    unit = dq[_KEY_UNIT]
+                system = None
+                if _KEY_SYSTEM in dq:
+                    system = dq[_KEY_SYSTEM]
+                code = None
+                if _KEY_CODE in dq:
+                    code = dq[_KEY_CODE]
+                dq_obj = DoseQuantityObj(value, unit, system, code)
+            if dq_obj is not None:
+                dosage_list.append(dq_obj)
+
+    return dosage_list
+
+                    
+###############################################################################
+def _decode_medication_statement(obj):
+    """
+    Decode a CQL Engine 'MedicationStatement' JSON object.    
+    """
+
+    if _TRACE: print('Decoding MedicationStatement resource...')
+
+    obj_type = type(obj)
+    assert dict == obj_type
+
+    id_value = _decode_id_value(obj)
+    subject_reference, subject_display = _decode_subject_info(obj)
+    context_reference = _decode_context_info(obj)    
+    code_systems_list = _decode_code_dict(obj)
+
+    date_time = None
+    end_date_time = None
+    if _KEY_EFF_PERIOD in obj:
+        eff_period_obj = obj[_KEY_EFF_PERIOD]
+        date_time, end_date_time = _decode_effective_period(eff_period_obj)
+
+    taken = False
+    if _KEY_TAKEN in obj:
+        taken_char = obj[_KEY_TAKEN]
+        if 'y' == taken_char:
+            taken = True
+
+    dosage_list = _decode_dosage(obj)
+            
+    med_stmt = MedicationStatementResource(
+        id_value = id_value,
+        context_reference = context_reference,
+        medication_codeable_concept = code_systems_list,
+        subject_reference = subject_reference,
+        subject_display = subject_display,
+        taken = taken,
+        dosage_list = dosage_list,
+        date_time = date_time,
+        end_date_time = end_date_time
+    )
+
+    return med_stmt
 
 
 ###############################################################################
@@ -508,6 +631,9 @@ def _decode_bundle(name, bundle_obj):
             elif _STR_CONDITION == resource_type_str:
                 condition = _decode_condition(elt)
                 bundled_objs.append(condition)
+            elif _STR_MEDICATION_STATEMENT == resource_type_str:
+                med_statement = _decode_medication_statement(elt)
+                bundled_objs.append(med_statement)
     
     return bundled_objs
 
@@ -538,7 +664,7 @@ def decode_top_level_obj(obj):
             if _STR_PATIENT == result_type_str:
                 result_obj = _decode_patient(name, result_obj)
                 if _TRACE: print('decoded patient')
-            elif _STR_BUNDLE == result_type_str:
+            elif _STR_BUNDLE2 == result_type_str or _STR_BUNDLE3 == result_type_str:
                 result_obj = _decode_bundle(name, result_obj)
             else:
                 if _TRACE: print('no decode')
@@ -548,3 +674,46 @@ def decode_top_level_obj(obj):
         assert False
 
     return result_obj
+
+
+###############################################################################
+def _get_version():
+    return '{0} {1}.{2}'.format(_MODULE_NAME, _VERSION_MAJOR, _VERSION_MINOR)
+
+
+###############################################################################
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(
+        description='Decode results from the CQL Engine')
+
+    parser.add_argument('-v', '--version',
+                        action='store_true',
+                        help='show the version string and exit')
+    parser.add_argument('-f', '--filepath',
+                        help='path to JSON file containing CQL Engine results')
+
+    args = parser.parse_args()
+
+    if 'version' in args and args.version:
+        print(_get_version())
+        sys.exit(0)
+
+    filepath = None
+    if 'filepath' in args and args.filepath:
+        filepath = args.filepath
+        if not os.path.isfile(filepath):
+            print('Unknown file specified: "{0}"'.format(filepath))
+            sys.exit(-1)
+    
+    with open(filepath, 'rt') as infile:
+        json_string = infile.read()
+        json_data = json.loads(json_string)
+
+        for obj in json_data:
+            result = decode_top_level_obj(obj)
+            if result is not None:
+                for elt in result:
+                    print(elt)
+        
+        
