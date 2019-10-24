749e1f2a5d6242f9b32bf8b0f805518b5d39e43a
Charity Hilton <Charity.Hilton@gtri.gatech.edu>
2019-10-23 21:16:13 -0400

    format logging for new lines

--- nlp/claritynlp_logging.py
+++ nlp/claritynlp_logging.py
@@ -19,7 +19,20 @@ def log(obj='', level=INFO, file=sys.stdout):
     the_caller = the_stack[0].f_locals.get('self', None)
 
     if isinstance(obj, Exception):
-        obj = '\n'.join(traceback.format_tb(obj.__traceback__))
+        log("EXCEPTION: {}".format(repr(obj), level=ERROR))
+        for t in traceback.format_tb(obj.__traceback__):
+            lines = t.split('\n')
+            for l in lines:
+                if l.strip() == '':
+                    continue
+                log("     {}".format(l), level=ERROR)
+        return
+
+    repr_obj = repr(obj)
+    if '\n' in repr_obj:
+        for l in repr_obj.split('\n'):
+            log(l, level=level)
+        return
 
     if the_caller:
         the_caller = "({}) ".format(repr(the_caller.__class__.__name__))
@@ -31,7 +44,7 @@ def log(obj='', level=INFO, file=sys.stdout):
             file = sys.stderr
 
     if the_app:
-        message = "{}{}".format(the_caller, repr(obj))
+        message = "{}{}".format(the_caller, repr_obj)
         if level == DEBUG:
             the_app.logger.debug(message)
         elif level == WARNING:
@@ -43,7 +56,7 @@ def log(obj='', level=INFO, file=sys.stdout):
         else:
             the_app.logger.info(message)
     else:
-        message = repr(obj)
+        message = repr_obj
         the_time = strftime("%Y-%m-%d %H:%M:%S-%Z", localtime())
         print("[{}] {} in claritynlp_logging: {}{}".format(the_time, level, the_caller, message), file=file)
 
