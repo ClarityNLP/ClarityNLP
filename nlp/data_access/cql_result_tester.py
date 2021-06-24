140174b92018763a0ce7066b5d7fa49f87c328da
Richard Boyd <richard.boyd@gtri.gatech.edu>
2021-06-24 14:29:12 -0400

    updated to handle latest CQL Engine results, fixed a few other problems also

--- nlp/data_access/cql_result_tester.py
+++ nlp/data_access/cql_result_tester.py
@@ -1,19 +1,22 @@
 #!/usr/bin/env python3
 """
+Interactive test program for the cql_result_parser.
 """
 
 import re
 import os
 import sys
 import json
-import optparse
+import argparse
 import cql_result_parser as crp
 from collections import namedtuple
-from claritynlp_logging import log, ERROR, DEBUG
 
 _VERSION_MAJOR = 0
-_VERSION_MINOR = 1
-_MODULE_NAME   = 'cibmtr_tester.py'
+_VERSION_MINOR = 2
+_MODULE_NAME   = 'cql_result_tester.py'
+
+# set to True to enable debug output
+_TRACE = True
 
 
 ###############################################################################
@@ -21,17 +24,75 @@ def _enable_debug():
 
     crp.enable_debug()
 
-        
+
+###############################################################################
+def _get_maxlen(obj):
+
+    return max([len(k) for k,v in obj.items()])
+    
+###############################################################################
+def _print_results(obj, maxlen=None):
+
+    if obj is None:
+        return
+
+    obj_type = type(obj)
+
+    # find the max key length - do it once
+    if maxlen is None:
+        if dict == obj_type:
+            maxlen = _get_maxlen(obj)
+        elif list == obj_type:
+            maxlen = 0
+            for item in obj:
+                l = _get_maxlen(item)
+                if l > maxlen:
+                    maxlen = l
+    
+    if dict == obj_type:
+        for k,v in obj.items():
+            # replace any embedded tabs and newlines with single space
+            if str == type(v):
+                v = re.sub(r'[\t\n]', ' ', v)
+                v = re.sub(r'\s+', ' ', v)
+            print('{0:>{1}}: {2}'.format(k, maxlen, v))
+        print()
+    elif list == obj_type:
+        for item in obj:
+            # recursive call
+            _print_results(item, maxlen)
+    else:
+        print('cql_result_tester: Unexpected object type: {0}'.format(obj_type))
+        print(obj)
+        assert False
+
+    
 ###############################################################################
 def _run(json_obj):
 
+    if _TRACE:
+        print('Decoding JSON response from CQL engine...\n')
+    
+    KEY_RESULT_TYPE = 'resultType'
+
+    # this code should mimic that in CQLExecutionTask::_json_to_objs
+    
     results = []
+
+    obj_count = len(json_obj)
     
     # assumes we either have a list of objects or a single obj
     obj_type = type(json_obj)
+
     if list == obj_type:
-        #log('**** SAW A LIST ****')
+        if _TRACE:
+            print('Found list of length {0}:'.format(obj_count))
         for e in json_obj:
+            # skip if no resultType key
+            if KEY_RESULT_TYPE not in e:
+                continue
+            if _TRACE:
+                print('\tDecoding resource type {0}'.format(e['resultType']))
             result_obj = crp.decode_top_level_obj(e)
             if result_obj is None:
                 continue
@@ -40,20 +101,19 @@ def _run(json_obj):
             else:
                 results.extend(result_obj)
     elif dict == obj_type:
-        #log('**** SAW A DICT ****')
+        if _TRACE:
+            print('Found dict of size {0}'.format(obj_count))
         result_obj = crp.decode_top_level_obj(json_obj)
         if result_obj is not None:
             if list is not type(result_obj):
                 results.append(result_obj)
             else:
                 results.extend(result_obj)
+    
+    print('\ncql_result_tester: Fully decoded {0} of {1} objects.\n'.
+          format(len(results), obj_count))
 
-    #log('found {0} results'.format(len(results)))
-
-    counter = 0
-    for obj in results:
-        log('Result {0}: {1}'.format(counter, obj))
-        counter += 1
+    _print_results(results)
         
         
 ###############################################################################
@@ -63,8 +123,8 @@ def _get_version():
 
 ###############################################################################
 def _show_help():
-    log(_get_version())
-    log("""
+    print(_get_version())
+    print("""
     USAGE: python3 ./{0} -f <json_file> [-hvd]
 
     REQUIRED:
@@ -82,45 +142,46 @@ def _show_help():
 ###############################################################################
 if __name__ == '__main__':
 
-    optparser = optparse.OptionParser(add_help_option=False)
-    optparser.add_option('-f', '--file', action='store',
-                         dest='filepath')
-    optparser.add_option('-v', '--version', action='store_true',
-                         dest='get_version')
-    optparser.add_option('-d', '--debug', action='store_true',
-                         dest='debug', default=False)
-    optparser.add_option('-h', '--help', action='store_true',
-                         dest='show_help', default=False)
-    #optparser.add_option('-z', '--selftest', action='store_true',
-    #                     dest='selftest', default=False)
+    parser = argparse.ArgumentParser(
+        description='Decode the CQL Engine JSON result file.'
+    )
 
-    opts, other = optparser.parse_args(sys.argv)
+    parser.add_argument('-v', '--version',
+                        action='store_true',
+                        help='show the version string and exit')
+    parser.add_argument('-f', '--file',
+                        dest='filepath',
+                        help='path to JSON file containing CQL Engine results')
+    parser.add_argument('-d', '--debug',
+                        action='store_true',
+                        help='pint debug info to stdout')
 
-    if 1 == len(sys.argv) or opts.show_help:
-        _show_help()
+    args = parser.parse_args()
+
+    if 'version' in args and args.version:
+        print(_get_version())
         sys.exit(0)
-
-    if opts.get_version:
-        log(get_version())
-        sys.exit(0)
-
-    if opts.debug:
+    
+    if 'debug' in args and args.debug:
        _enable_debug()
 
-    json_file = opts.filepath
-    if not os.path.isfile(json_file):
-        log("File not found: '{0}'".format(json_file))
+    json_file = None
+    if 'filepath' in args and args.filepath:
+        json_file = args.filepath
+        if not os.path.isfile(json_file):
+            print("File not found: '{0}'".format(json_file))
+            sys.exit(-1)
+    else:
+        print('Required "--file" argument not found.')
         sys.exit(-1)
 
     try:
         infile = open(json_file, 'rt')
         file_data = json.load(infile)
     except Exception as e:
-        log("Could not load file contents{0}.".format(json_file))
-        log(e)
+        print("Could not load file contents{0}.".format(json_file))
+        print(e)
         sys.exit(-1)
 
     infile.close()
-
-    #log(json.dumps(file_data, indent=4))
     _run(file_data)
