049334fb69f9c22b0fc6e205fdd0d0b4b1243830
Charity A Hilton <Charity.Hilton@gtri.gatech.edu>
2022-01-28 19:50:21 -0500

    add smarthub users to setup

--- utilities/ingest-mongo/users.sh
+++ utilities/ingest-mongo/users.sh
@@ -4,4 +4,6 @@ var passwd = '$MONGO_INITDB_ROOT_PASSWORD';
 var admin = db.getSiblingDB('admin');
 admin.auth(user, passwd);
 db.createUser({user: user, pwd: passwd, roles: ["dbOwner"]});
+use $SMARTHUB_MONGO_DATABASE;
+db.createUser({ user: "$SMARTHUB_MONGO_USERNAME", pwd: "$SMARTHUB_MONGO_PASSWORD", roles: [ { role: "readWrite", db: "$SMARTHUB_MONGO_DATABASE" } ]});
 EOF
