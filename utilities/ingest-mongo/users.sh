622cb4a3866ada4a67b9f525de4dfef8960a771b
Charity A Hilton <Charity.Hilton@gtri.gatech.edu>
2022-02-02 14:59:16 -0500

    Update users.sh

--- utilities/ingest-mongo/users.sh
+++ utilities/ingest-mongo/users.sh
@@ -3,11 +3,7 @@ var user = '$MONGO_INITDB_ROOT_USERNAME';
 var passwd = '$MONGO_INITDB_ROOT_PASSWORD';
 var admin = db.getSiblingDB('admin');
 admin.auth(user, passwd);
-if (db.getUser(user) == null) { 
-  db.createUser({user: user, pwd: passwd, roles: ["dbOwner"]});
-}
-if (db.getUser("$MONGO_SMARTHUB_USERNAME") == null) { 
-  use $MONGO_SMARTHUB_DATABASE;
-  db.createUser({ user: "$MONGO_SMARTHUB_USERNAME", pwd: "$MONGO_SMARTHUB_PASSWORD", roles: [ { role: "readWrite", db: "$MONGO_SMARTHUB_DATABASE" } ]});
-}
+db.createUser({user: user, pwd: passwd, roles: ["dbOwner"]});
+use $MONGO_SMARTHUB_DATABASE;
+db.createUser({ user: "$MONGO_SMARTHUB_USERNAME", pwd: "$MONGO_SMARTHUB_PASSWORD", roles: [ { role: "readWrite", db: "$MONGO_SMARTHUB_DATABASE" } ]});
 EOF
