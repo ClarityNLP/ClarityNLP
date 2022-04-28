8a010b90b5c2d41dc1dd14358307c67628bf4d51
Richard Boyd <richard.boyd@gtri.gatech.edu>
2022-04-28 17:07:35 -0400

    more work on education regexes and identifying the education level and degree

--- nlp/algorithms/finder/education_finder.py
+++ nlp/algorithms/finder/education_finder.py
@@ -23,9 +23,45 @@ try:
     import finder_overlap as overlap
 except:
     from algorithms.finder import finder_overlap as overlap
+    
+
+EDUC_ELEM         = 'less than high school'
+EDUC_SOME_HS      = 'some high school'
+EDUC_HS_GED       = 'high school diploma or GED'
+EDUC_SOME_COLLEGE = 'some college'
+EDUC_COLLEGE_DEG  = 'college degree'
+    
+EDUC_TUPLE_FIELDS = [
+    'sentence',
+    'education_level'
+]
+
+EducTuple = namedtuple('EducTuple', EDUC_TUPLE_FIELDS)
+EducTuple.__new__.__defaults__ = (None,) * len(EducTuple._fields)
 
 
-_str_degrees = r'((ph|sc)\.?d\.?|[bm]\.?[asd]\.?|s\.?[bm]\.?)'
+###############################################################################
+
+_VERSION_MAJOR = 0
+_VERSION_MINOR = 2
+
+# set to True to enable debug output
+_TRACE = False
+
+#_str_degrees = r'\b(?P<degree>(ged|(ph|sc)\.?d\.?|[bm]\.?[asd]\.?|s\.?[bm]\.?)|masters?|batchelors?|doctor(al|s))\b'
+# regex capture group names
+_DEG_DOCTORAL   = 'doctoral'
+_DEG_MASTERS    = 'masters'
+_DEG_BATCHELORS = 'batchelors'
+_DEG_GED        = 'ged'
+_str_doctoral = r'\b(doctor(al|ate|s)|(ph|sc)\.?d\.?|m\.?d\.?)\b'
+_str_masters = r'\b(masters?|m\.?[as]\.?|s\.?[bm]\.?)\b'
+_str_batchelors = r'\b(batchelors?|b\.?[as]\.?|s\.?[ab]\.?)\b'
+_str_ged = r'\bged\b'
+_str_degrees = r'(' + r'(?P<doctoral>' + _str_doctoral + r')|' +\
+    r'(?P<masters>' + _str_masters + r')|' +\
+    r'(?P<batchelors>' + _str_batchelors + r')|' +\
+    r'(?P<ged>(' + _str_ged + r')))'
 _regex_degrees = re.compile(_str_degrees, re.IGNORECASE)
 
 # a word, possibly hyphenated or abbreviated
@@ -48,7 +84,14 @@ _str_jhs = r'\b(junior|j\.?r\.?) ' + _str_hs
 _str_college = r'\b(college|university|grad(uate)? school)\b'
 _str_named_year = r'\b(fresh(man)?|soph(o?more)?|(junior|j\.?r)|(senior|s\.?r))\.?\b'
 
-_str_school = r'(?P<school>(' + _str_college + r'|' + _str_hs + r'|' + _str_jhs + r'|' + _str_elem + r'))'
+#_str_school = r'(?P<school>(' + _str_college + r'|' + _str_hs + r'|' + _str_jhs + r'|' + _str_elem + r'))'
+# regex capture group names
+_SCHOOL_COLLEGE = 'college'
+_SCHOOL_HS      = 'hs'
+_SCHOOL_ELEM    = 'elem'
+_str_school = r'(' + r'(?P<college>' + _str_college + r')|' +\
+    r'(?P<hs>' + _str_hs + r')|' +\
+    r'(?P<elem>' + r'(' + _str_jhs + r')|(' + _str_elem + r')))'
 
 # some school
 _str_some1 = r'\b(attend(ing|ed|s)|began|dropped out|(never|did not|didnt) finish(ed)?|some|in)' +\
@@ -59,36 +102,55 @@ _regex_some1 = re.compile(_str_some1, re.IGNORECASE)
 _str_some2 = _str_named_year + _str_words + _str_school
 _regex_some2 = re.compile(_str_some2, re.IGNORECASE)
 
-# ... is a school student...
-_str_some3 = _str_school + _str_words + r'\bstudent\b'
+# ...is a high school junior
+_str_some3 = _str_school + r' ' + _str_named_year
 _regex_some3 = re.compile(_str_some3, re.IGNORECASE)
+
+# ... is a school student...
+_str_some4 = _str_school + _str_words + r'\bstudent\b'
+_regex_some4 = re.compile(_str_some4, re.IGNORECASE)
 
 # ...high school dropout...
 _str_drop1 = _str_school + _str_words + r'\bdropout\b'
 _regex_drop1 = re.compile(_str_drop1, re.IGNORECASE)
 
 # ...dropped out of college...
-_str_drop2 = r'\bdropped out of' + _str_words + _str_school + _str_words + _str_named_year
+_str_dropped_out = r'\b(dropped out of|did not (complete|finish)|quit|left)\b'
+_str_drop2 = _str_dropped_out + _str_words + _str_school + r'(' + _str_words + _str_named_year + r'( year)?' + r')?'
 _regex_drop2 = re.compile(_str_drop2, re.IGNORECASE)
 
 # ...graduated from high school...
-_str_grad1 = r'\b(?<!never )(graduated( from)?|completed|finished)\b' + _str_words + _str_school
+_str_grad1 = r'\b(?<!never )(graduated( from)?|completed|finished)(?! some)\b' + _str_words + _str_school
 _regex_grad1 = re.compile(_str_grad1, re.IGNORECASE)
 
 # ...is a college graduate...
 _str_grad2 = r'\bis a\b' + _str_words + _str_school + _str_words + r'\bgraduate\b'
 _regex_grad2 = re.compile(_str_grad2, re.IGNORECASE)
 
-# ...has a college degree...
-_str_grad3 = r'\b(received|earned|completed|finished|has)\b' + _str_words +\
+# ...received a college degree...
+_str_received = r'\b(received|recieved|earned|completed|finished|obtained|did get|getting|got|has|with)(?! some)\b'
+_str_grad3 = _str_received + _str_words +\
     _str_school + _str_words + r'\b(degree|diploma|education|certificat(ion|e))s?\b'
 _regex_grad3 = re.compile(_str_grad3, re.IGNORECASE)
 
+# ...received a <specific degree>...
+_str_grad4 = _str_received + _str_words + _str_degrees
+_regex_grad4 = re.compile(_str_grad4, re.IGNORECASE)
+
+# Education: GED
+_str_terse = r'\b(education|social history) ' + _str_degrees
+_regex_terse = re.compile(_str_terse, re.IGNORECASE)
+
+
+# ...went to <college> for <degree>
+_str_specific_degree = r'\b(went to|attended|enrolled (at|in)|graduated from)\b' + _str_words + _str_college + _str_words + _str_degrees
+_regex_specific_degree = re.compile(_str_specific_degree, re.IGNORECASE)
 
 _REGEXES_SOME_SCHOOL = [
     _regex_some1,
     _regex_some2,
     _regex_some3,
+    _regex_some4,
 
     _regex_drop1,
     _regex_drop2,
@@ -98,19 +160,13 @@ _REGEXES_GRADUATED = [
     _regex_grad1,
     _regex_grad2,
     _regex_grad3,
+    _regex_grad4,
+    _regex_terse,
+    _regex_specific_degree,
 ]
 
 
 _CHAR_SPACE = ' '
-
-    
-###############################################################################
-
-_VERSION_MAJOR = 0
-_VERSION_MINOR = 2
-
-# set to True to enable debug output
-_TRACE = True
 
 
 ###############################################################################
@@ -154,13 +210,6 @@ def _regex_match(sentence, regex_list):
 
     sentence_save = sentence
     
-    # # erase any negated languages from the sentence, then attempt regexes
-    # neg_match = _regex_neg_language.search(sentence)
-    # if neg_match:
-    #     if _TRACE:
-    #         print('NEG LANGUAGE MATCH: "{0}"'.format(neg_match.group()))
-    #     sentence = sentence[:neg_match.start()] + sentence[neg_match.end():]
-    
     candidates = []
     for i, regex in enumerate(regex_list):
         iterator = regex.finditer(sentence)
@@ -170,45 +219,66 @@ def _regex_match(sentence, regex_list):
             start = match.start()
             end = start + len(match_text)
 
-            # isolate the school
-            school_text = match.group('school').strip()
+            # isolate the school, if any
+            school_text = None
+            if _SCHOOL_COLLEGE in match.groupdict() and match.group(_SCHOOL_COLLEGE) is not None:
+                school_text = _SCHOOL_COLLEGE
+            elif _SCHOOL_HS in match.groupdict() and match.group(_SCHOOL_HS) is not None:
+                school_text = _SCHOOL_HS
+            elif _SCHOOL_ELEM in match.groupdict() and match.group(_SCHOOL_ELEM) is not None:
+                school_text = _SCHOOL_ELEM
 
-            #print('\t{0}'.format(school_text))
+            degree_text = None
+            #if 'degree' in match.groupdict():
+            #    degree_text = match.group('degree').strip()
+            if _DEG_DOCTORAL in match.groupdict() and match.group(_DEG_DOCTORAL) is not None:
+                degree_text = _DEG_DOCTORAL
+            elif _DEG_MASTERS in match.groupdict() and match.group(_DEG_MASTERS) is not None:
+                degree_text = _DEG_MASTERS
+            elif _DEG_BATCHELORS in match.groupdict() and match.group(_DEG_BATCHELORS) is not None:
+                degree_text = _DEG_BATCHELORS
+            elif _DEG_GED in match.groupdict() and match.group(_DEG_GED) is not None:
+                degree_text = _DEG_GED
+
+            info_dict = {
+                'school' : school_text,
+                'degree' : degree_text,
+            }
             
             candidates.append(overlap.Candidate(
-                start, end, match_text, regex, other=school_text
+                start, end, match_text, regex, other=info_dict
             ))
 
     # sort the candidates in DECREASING order of length
     candidates = sorted(candidates, key=lambda x: x.end-x.start)
     return candidates
 
-    # if _TRACE:
-    #     print('\tCandidate matches: ')
-    #     index = 0
-    #     for c in candidates:
-    #         regex_index = regex_list.index(c.regex)
-    #         print('\t[{0:2}] R{1:2}\t[{2},{3}): ->{4}<-'.
-    #               format(index, regex_index, c.start, c.end, c.match_text))
-    #         index += 1
-    #     print()
+    if _TRACE:
+        print('\tCandidate matches: ')
+        index = 0
+        for c in candidates:
+            regex_index = regex_list.index(c.regex)
+            print('\t[{0:2}] R{1:2}\t[{2},{3}): ->{4}<-'.
+                  format(index, regex_index, c.start, c.end, c.match_text))
+            index += 1
+        print()
 
-    # # keep the longest of any overlapping matches
-    # pruned_candidates = overlap.remove_overlap(candidates,
-    #                                            False,
-    #                                            keep_longest=True)
+    # keep the longest of any overlapping matches
+    pruned_candidates = overlap.remove_overlap(candidates,
+                                               False,
+                                               keep_longest=True)
 
-    # if _TRACE:
-    #     print('\tCandidate matches after overlap resolution: ')
-    #     index = 0
-    #     for c in pruned_candidates:
-    #         regex_index = regex_list.index(c.regex)
-    #         print('\t[{0:2}] R{1:2}\t[{2},{3}): ->{4}<-'.
-    #               format(index, regex_index, c.start, c.end, c.match_text))
-    #         index += 1
-    #     print()
+    if _TRACE:
+        print('\tCandidate matches after overlap resolution: ')
+        index = 0
+        for c in pruned_candidates:
+            regex_index = regex_list.index(c.regex)
+            print('\t[{0:2}] R{1:2}\t[{2},{3}): ->{4}<-'.
+                  format(index, regex_index, c.start, c.end, c.match_text))
+            index += 1
+        print()
     
-    # return pruned_candidates
+    return pruned_candidates
 
 
 ###############################################################################
@@ -217,8 +287,8 @@ def run(sentence):
     results = []
     cleaned_sentence = _cleanup(sentence)
 
-    # if _TRACE:
-    #     print(cleaned_sentence)
+    if _TRACE:
+        print(cleaned_sentence)
 
     candidates1 = _regex_match(cleaned_sentence, _REGEXES_SOME_SCHOOL)
     print('SOME SCHOOL: ')
@@ -230,6 +300,8 @@ def run(sentence):
     for c in candidates2:
         print('\t{0}, {1}'.format(c.match_text, c.other))
 
+    # determine the highest level of education
+    
         
 ###############################################################################
 def get_version():
@@ -246,6 +318,7 @@ if __name__ == '__main__':
         'lives at home and attends [**Location (un) 1375**] High School',
         'Pt is a senior at [**Location (un) **] High School',
         'in high school and lives in [**Location 3356**] with his brother',
+        'This is an 18yr old high school senior admitted to 11R',        
         "I'm only in High School",
         'Pt is an 18 year old college student',        
         'Mother also attends high school in [**Name (NI) 21**] and is in the 11th grade',        
@@ -262,16 +335,19 @@ if __name__ == '__main__':
         'he dropped out of school in his senior year and is presently working',
         'he dropped out of school in 11th grade',
         'she has dropped out of high school',
+        'left high school and is studying to get a GED',        
         'fob is still attending high school',
         'he never finished high school',        
         'She states pt lives at home, he dropped out of high school 2 years ago and is unemployed',
         'High school graduate, some college',
         'He did complete some college',
         'Completed some college',
-        'Finished high school and took some college courses',        
+        'Finished high school and took some college courses',
+        'he has some college courses',
+        'Completed some college level education',        
 
         # neg
-        'There is also evidence of signal dropout on the gradient echo images'
+        'There is also evidence of signal dropout on the gradient echo images',
         'Faint area of susceptibility dropout on the GRE images is again noted',
         'Pt is an elementary school principal and lives with his youngest son',
         'found by police after breaking and entering into elementary school',
@@ -284,15 +360,50 @@ if __name__ == '__main__':
         print('\n' + sentence)
         results = run(sentence)
 
-
-    # graduated from a school
     SENTENCES_2  = [
+        # graduated from a school        
         'pt graduated high school',
         'he received a HS diploma',        
         'Pt. is a recent high school graduate',
         'pt does construction work and has a high school education',
         'mo completed high school and had planned to attend college when ',
-        "Pt recently graduated from high school and is working at Stop'n'Shop",        
+        "Pt recently graduated from high school and is working at Stop'n'Shop",
+        
+
+        # has a specific degree
+        'she has a Ph.D in chemistry from MIT',
+        "later went to grad school for Master's in French Lit.",        
+        'Pt has a GED, has worked multiple small jobs through out his life',
+        'She has a tenth grade education with a GED.',
+        'The patient has a 10th grade education and then obtained his GED.',
+        'With his parents support, he quit HS and pursued a GED because of the ' \
+        'academic stress he experienced in school',
+        'He later obtained his GED',        
+        'obtained a GED and attended some college',
+        'Reportedly earned GED.',
+        'He received his GED while in prison',
+        'Pt. did not complete H. S., but did get his GED, and then entered NAVY',
+        'is being told, even with a GED, that \"she is over\n   qualified.\"',
+        'Dropped out of [**Location (un) **] HS and got his GED.',
+        'He recieved his GED and was in the National Guard for 24 years',
+        'He has a GED level of education',
+        'dropped out of high school, got his GED',
+        'reports getting a GED',
+        'Education- GED;',
+        'Education: GED',
+        'She attended high school through the 11th grade, obtained a GED and attended some college',        
+
+        # no match
+        'would eventually like to get his GED',
+        'Going to get his GED and eventually hopes to work for the EPA',
+        'With the microcatheter in this position a series of GED MRI-compatible '\
+        'coils were placed to treat the aneurysm',
+        'prior to this illness had been attending night school to get her GED',
+        'pt electively intubated for GED procedure',
+        'Underwent GED, dubhoff tube placed',
+
+        'one of her goals remains to finish high school',
+        'exploring option of returning to school and taking some college courses',
         
     ]
 
@@ -300,59 +411,3 @@ if __name__ == '__main__':
         print('\n' + sentence)
         results = run(sentence)
         
-    
-    SENTENCES = [
-        'allow pt. to attend his High School graduation tomorrow',        
-        'she has a Ph.D in chemistry from MIT',
-        'This is an 18yr old high school senior admitted to 11R',
-        'He is second to the youngest and due to graduate High School this Weekend',
-
-        "later went to grad school for Master's in French Lit.",
-
-        'He played football and baseball in high school and then played football ' \
-        'in his first (and only) semester of college',
-        
-        'one of her goals remains to finish high school',
-        'patient has been very unhappy in her degree program',
-        
-
-        'he could not pass a GED exam',
-        'is being told, even with a GED, that \"she is over\n   qualified.\"',
-        'Occupation: studying for GED',
-        'Pt has a GED, has worked multiple small jobs through out his life',
-        'With his parent\ns support, he quit HS and pursued a GED because of the ' \
-        'academic stress he experienced in school',
-        'With the microcatheter in this position a series of GED MRI-compatible '\
-        'coils were placed to treat the aneurysm',
-        'She has a tenth grade education with a GED.',
-        'The patient has a 10th grade education and then obtained his GED.',
-        'Reportedly earned GED.',
-        'He later obtained his GED',
-        'obtained a GED and attended some college',
-        'He received his GED while in prison',
-        'Dropped out of [**Location (un) **] HS and got his GED.',
-        'Pt. did not complete H. S., but did get his GED, and then entered NAVY',
-        'studying for final step of GED',
-        'Going to school for his GED',
-        'He recieved his GED and was in the National Guard for 24 years',
-        'She has a GED',
-        'He has a GED level of education',
-        'dropped out of high school, got his GED',
-        'reports getting a GED',
-        'left high school and is studying to get a GED',
-        'Education- GED;',
-        'Education: GED',
-        'prior to this illness had been attending night school to get her GED',
-        'would eventually like to get his GED',
-        'Going to get his GED and eventually hopes to work for the EPA',
-        'pt electively intubated for GED procedure',
-        'Underwent GED, dubhoff tube placed',
-        
-
-        'She attended high school through the 11th grade, obtained a GED and attended some college',
-        'exploring option of returning to school and taking some college courses',
-        'he has some college courses',
-        'Completed some college level education',
-
-        
-    ]
