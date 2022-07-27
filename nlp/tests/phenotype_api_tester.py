41ce1bc522033b8f9abc816f846a6caafa908774
Charity Hilton <charity.a.hilton@gmail.com>
2022-07-27 11:02:16 -0400

    support for simultaneous in-memory jobs

---
+++ nlp/tests/phenotype_api_tester.py
@@ -0,0 +1,13 @@
+try:
+    from .data_access import *
+    from .apis import *
+except Exception:
+    from apis import *
+    from data_access import *
+
+
+if __name__ == "__main__":
+    p_cfg =
+    post_phenotype(
+
+    )
